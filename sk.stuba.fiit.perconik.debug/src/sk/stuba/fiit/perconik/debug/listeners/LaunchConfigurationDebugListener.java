29bf031bb61937d908b9dcfd8939be36827c1c12
Pavol Zbell <pavol.zbell@gmail.com>
2013-07-26 12:09:19 +0200

    Refactor launch and launch configuration listeners

--- sk.stuba.fiit.perconik.debug/src/sk/stuba/fiit/perconik/debug/listeners/LaunchConfigurationDebugListener.java
+++ sk.stuba.fiit.perconik.debug/src/sk/stuba/fiit/perconik/debug/listeners/LaunchConfigurationDebugListener.java
@@ -1,8 +1,14 @@
 package sk.stuba.fiit.perconik.debug.listeners;
 
+import java.util.Set;
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.runtime.CoreException;
 import org.eclipse.debug.core.ILaunchConfiguration;
+import org.eclipse.debug.core.ILaunchConfigurationType;
 import sk.stuba.fiit.perconik.core.listeners.LaunchConfigurationListener;
 import sk.stuba.fiit.perconik.core.utilities.PluginConsole;
+import sk.stuba.fiit.perconik.core.utilities.SmartStringBuilder;
+import com.google.common.collect.ImmutableList;
 
 public final class LaunchConfigurationDebugListener extends AbstractDebugListener implements LaunchConfigurationListener
 {
@@ -17,13 +23,90 @@ public final class LaunchConfigurationDebugListener extends AbstractDebugListene
 	
 	public final void launchConfigurationAdded(final ILaunchConfiguration configuration)
 	{
+		this.print("Launch added:");
+		this.printLaunchConfiguration(configuration);
 	}
 
 	public final void launchConfigurationRemoved(final ILaunchConfiguration configuration)
 	{
+		this.print("Launch removed:");
+		this.printLaunchConfiguration(configuration);
 	}
 
 	public final void launchConfigurationChanged(final ILaunchConfiguration configuration)
 	{
+		this.print("Launch changed:");
+		this.printLaunchConfiguration(configuration);
+	}
+	
+	private final void printLaunchConfiguration(final ILaunchConfiguration configuration)
+	{
+		try
+		{
+			this.put(dumpLaunchConfiguration(configuration));
+		}
+		catch (CoreException e)
+		{
+			error("Launch configuration error", e);
+		}
+	}
+	
+	static final String dumpLaunchConfiguration(final ILaunchConfiguration configuration) throws CoreException
+	{
+		SmartStringBuilder builder = new SmartStringBuilder().tab();
+		
+		ILaunchConfigurationType type = configuration.getType();
+		IFile file = configuration.getFile();
+		
+		String      name     = configuration.getName();
+		String      category = configuration.getCategory();
+		Set<String> modes    = configuration.getModes();
+		
+		String  application = configuration.getAttribute("application", "?");
+		String  product     = configuration.getAttribute("product", "?");
+		boolean useProduct  = configuration.getAttribute("useProduct", false);
+		
+		builder.append("name: ").appendln(name);
+		builder.append("category: ").appendln(category);
+		builder.appendln("type:").lines(dumpLaunchConfigurationType(type));
+		builder.append("modes: ").list(modes.isEmpty() ? ImmutableList.of("none") : modes).appendln();
+		
+		if (file != null)
+		{
+			builder.append("full path: ").appendln(file.getFullPath());
+			builder.append("location: ").appendln(file.getLocation());
+			builder.append("URI: ").appendln(file.getLocationURI());
+		}
+		
+		builder.append("application: ").appendln(application);
+		builder.append("product: ").append(product).append(" (use ").append(useProduct).appendln(")");
+		
+//		Map<Object, Object> attributes = configuration.getAttributes();
+//		
+//		for (Entry<Object, Object> entry: attributes.entrySet())
+//		{
+//			builder.append(entry.getKey()).append(": ").appendln(entry.getValue());
+//		}
+
+		return builder.toString();
+	}
+
+	static final String dumpLaunchConfigurationType(final ILaunchConfigurationType type)
+	{
+		SmartStringBuilder builder = new SmartStringBuilder().tab();
+		
+		String name             = type.getName();
+		String category         = type.getCategory();
+		String identifier       = type.getIdentifier();
+		String pluginIdentifier = type.getPluginIdentifier();
+		String contributorName  = type.getContributorName();
+
+		builder.append("name: ").appendln(name);
+		builder.append("category: ").appendln(category);
+		builder.append("identifier: ").appendln(identifier);
+		builder.append("plugin identifier: ").appendln(pluginIdentifier);
+		builder.append("contributor name: ").appendln(contributorName);
+		
+		return builder.toString();
 	}
 }
--- sk.stuba.fiit.perconik.debug/src/sk/stuba/fiit/perconik/debug/listeners/LaunchDebugListener.java
+++ sk.stuba.fiit.perconik.debug/src/sk/stuba/fiit/perconik/debug/listeners/LaunchDebugListener.java
@@ -1,12 +1,8 @@
 package sk.stuba.fiit.perconik.debug.listeners;
 
-import java.util.Set;
-import org.eclipse.core.resources.IFile;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.debug.core.ILaunch;
 import org.eclipse.debug.core.ILaunchConfiguration;
-import org.eclipse.debug.core.ILaunchConfigurationType;
-import com.google.common.collect.ImmutableList;
 import sk.stuba.fiit.perconik.core.listeners.LaunchListener;
 import sk.stuba.fiit.perconik.core.utilities.PluginConsole;
 import sk.stuba.fiit.perconik.core.utilities.SmartStringBuilder;
@@ -42,10 +38,17 @@ public final class LaunchDebugListener extends AbstractDebugListener implements 
 	
 	private final void printLaunch(final ILaunch launch)
 	{
-		this.put(dumpLaunch(launch));
+		try
+		{
+			this.put(dumpLaunch(launch));
+		}
+		catch (CoreException e)
+		{
+			error("Launch error", e);
+		}
 	}
 	
-	private final String dumpLaunch(final ILaunch launch)
+	static final String dumpLaunch(final ILaunch launch) throws CoreException
 	{
 		SmartStringBuilder builder = new SmartStringBuilder().tab();
 		
@@ -54,74 +57,8 @@ public final class LaunchDebugListener extends AbstractDebugListener implements 
 		String  mode = launch.getLaunchMode();
 		
 		builder.append("mode: ").appendln(mode);
-		builder.appendln("configuration:").lines(dumpLaunchConfiguration(configuration));
+		builder.appendln("configuration:").lines(LaunchConfigurationDebugListener.dumpLaunchConfiguration(configuration));
 
-		return builder.toString();
-	}
-	
-	private final String dumpLaunchConfiguration(final ILaunchConfiguration configuration)
-	{
-		SmartStringBuilder builder = new SmartStringBuilder().tab();
-		
-		try
-		{
-			ILaunchConfigurationType type = configuration.getType();
-			IFile file = configuration.getFile();
-			
-			String      name     = configuration.getName();
-			String      category = configuration.getCategory();
-			Set<String> modes    = configuration.getModes();
-			
-			String  application = configuration.getAttribute("application", "?");
-			String  product     = configuration.getAttribute("product", "?");
-			boolean useProduct  = configuration.getAttribute("useProduct", false);
-			
-			builder.append("name: ").appendln(name);
-			builder.append("category: ").appendln(category);
-			builder.appendln("type:").lines(dumpLaunchConfigurationType(type));
-			builder.append("modes: ").list(modes.isEmpty() ? ImmutableList.of("none") : modes).appendln();
-			
-			if (file != null)
-			{
-				builder.append("full path: ").appendln(file.getFullPath());
-				builder.append("location: ").appendln(file.getLocation());
-				builder.append("URI: ").appendln(file.getLocationURI());
-			}
-			
-			builder.append("application: ").appendln(application);
-			builder.append("product: ").append(product).append(" (use ").append(useProduct).appendln(")");
-			
-//			Map<Object, Object> attributes = configuration.getAttributes();
-//			
-//			for (Entry<Object, Object> entry: attributes.entrySet())
-//			{
-//				builder.append(entry.getKey()).append(": ").appendln(entry.getValue());
-//			}
-		}
-		catch (CoreException e)
-		{
-			error("Launch configuration error", e);
-		}
-
-		return builder.toString();
-	}
-
-	private final String dumpLaunchConfigurationType(final ILaunchConfigurationType type)
-	{
-		SmartStringBuilder builder = new SmartStringBuilder().tab();
-		
-		String name             = type.getName();
-		String category         = type.getCategory();
-		String identifier       = type.getIdentifier();
-		String pluginIdentifier = type.getPluginIdentifier();
-		String contributorName  = type.getContributorName();
-
-		builder.append("name: ").appendln(name);
-		builder.append("category: ").appendln(category);
-		builder.append("identifier: ").appendln(identifier);
-		builder.append("plugin identifier: ").appendln(pluginIdentifier);
-		builder.append("contributor name: ").appendln(contributorName);
-		
 		return builder.toString();
 	}
 }
