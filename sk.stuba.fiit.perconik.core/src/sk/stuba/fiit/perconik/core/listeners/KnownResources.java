67bda90ac10297b9d6b39852e295afe51495e7d9
Pavol Zbell <pavol.zbell@gmail.com>
2013-07-30 15:47:26 +0200

    Register resources for recently added listeners

--- sk.stuba.fiit.perconik.core/src/sk/stuba/fiit/perconik/core/listeners/KnownResources.java
+++ sk.stuba.fiit.perconik.core/src/sk/stuba/fiit/perconik/core/listeners/KnownResources.java
@@ -27,32 +27,56 @@ ILaunchesListener2
 
 class KnownResources
 {
+	static final Resource<ElementChangedListener> elementChanged;
+
 	static final Resource<FileBufferListener> fileBuffer;
 
 	static final Resource<LaunchListener> launch;
 	
 	static final Resource<LaunchConfigurationListener> launchConfiguration;
 	
+	static final Resource<LaunchesListener> launches;
+
 	static final Resource<OperationHistoryListener> operationHistory;
 	
 	static final Resource<PageListener> page;
 	
 	static final Resource<PartListener> part;
 	
+	static final Resource<PerspectiveListener> perspective;
+
 	static final Resource<RefactoringExecutionListener> refactoringExecution;
+
+	static final Resource<RefactoringHistoryListener> refactoringHistory;
+
+	static final Resource<ResourceChangeListener> resourceChange;
 
 	static final Resource<SelectionListener> selection;
 	
+	static final Resource<TestRunListener> testRun;
+
+	static final Resource<WindowListener> window;
+
+	static final Resource<WorkbenchListener> workbench;
+
 	static
 	{
+		elementChanged       = build(ElementChangedListener.class, ElementChangedHandler.INSTANCE);
 		fileBuffer           = build(FileBufferListener.class, FileBufferHandler.INSTANCE);
 		launch               = build(LaunchListener.class, LaunchHandler.INSTANCE);
 		launchConfiguration  = build(LaunchConfigurationListener.class, LaunchConfigurationHandler.INSTANCE);
+		launches             = build(LaunchesListener.class, LaunchesHandler.INSTANCE);
 		operationHistory     = build(OperationHistoryListener.class, OperationHistoryHandler.INSTANCE);
 		page                 = build(PageListener.class, PageHandler.INSTANCE);
 		part                 = build(PartListener.class, PartHandler.INSTANCE);
+		perspective          = build(PerspectiveListener.class, PerspectiveHandler.INSTANCE);
 		refactoringExecution = build(RefactoringExecutionListener.class, RefactoringExecutionHandler.INSTANCE);
+		refactoringHistory   = build(RefactoringHistoryListener.class, RefactoringHistoryHandler.INSTANCE);
+		resourceChange       = build(ResourceChangeListener.class, ResourceChangeHandler.INSTANCE);
 		selection            = build(SelectionListener.class, SelectionHandler.INSTANCE);
+		testRun              = build(TestRunListener.class, TestRunHandler.INSTANCE);
+		window               = build(WindowListener.class, WindowHandler.INSTANCE);
+		workbench            = build(WorkbenchListener.class, WorkbenchHandler.INSTANCE);
 	}
 	
 	private KnownResources()
