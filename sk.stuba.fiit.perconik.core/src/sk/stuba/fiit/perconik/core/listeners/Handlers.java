6b7a83244afd2b6d0af03c3657f605e904c6d553
Pavol Zbell <pavol.zbell@gmail.com>
2013-07-30 15:17:00 +0200

    Improve handlers internal API

--- sk.stuba.fiit.perconik.core/src/sk/stuba/fiit/perconik/core/listeners/Handlers.java
+++ sk.stuba.fiit.perconik.core/src/sk/stuba/fiit/perconik/core/listeners/Handlers.java
@@ -1,9 +1,17 @@
 package sk.stuba.fiit.perconik.core.listeners;
 
+import sk.stuba.fiit.perconik.core.utilities.IntegralConstant;
+import sk.stuba.fiit.perconik.core.utilities.IntegralConstantSupport;
+
 final class Handlers
 {
 	private Handlers()
 	{
 		throw new AssertionError();
+	}
+	
+	static final <E extends Enum<E> & IntegralConstant> int mask(final FilteringListener<E> listener)
+	{
+		return IntegralConstantSupport.constantsAsInteger(listener.getEventTypes());
 	}
 }
