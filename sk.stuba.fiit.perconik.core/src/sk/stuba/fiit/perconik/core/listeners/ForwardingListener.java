85b0ceac260f87cd4df14531b02845f19abc5c31
Pavol Zbell <pavol.zbell@gmail.com>
2013-07-30 14:19:13 +0200

    Add forwarding listener

---
+++ sk.stuba.fiit.perconik.core/src/sk/stuba/fiit/perconik/core/listeners/ForwardingListener.java
@@ -0,0 +1,29 @@
+package sk.stuba.fiit.perconik.core.listeners;
+
+import com.google.common.collect.ForwardingObject;
+
+public abstract class ForwardingListener extends ForwardingObject implements Listener
+{
+	@Override
+	protected abstract Listener delegate();
+
+	public void preRegister()
+	{
+		this.delegate().preRegister();
+	}
+
+	public void postRegister()
+	{
+		this.delegate().postRegister();
+	}
+
+	public void preUnregister()
+	{
+		this.delegate().preUnregister();
+	}
+
+	public void postUnregister()
+	{
+		this.delegate().postUnregister();
+	}
+}
