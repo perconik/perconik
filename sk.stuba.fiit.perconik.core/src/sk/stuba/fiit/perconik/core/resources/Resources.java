6a2ec1e56fd0af633076e25bac61da7b8df10a8f
Pavol Zbell <pavol.zbell@gmail.com>
2013-08-04 21:50:13 +0200

    Fix critical bug with default resource service, add smart service accessor

--- sk.stuba.fiit.perconik.core/src/sk/stuba/fiit/perconik/core/resources/Resources.java
+++ sk.stuba.fiit.perconik.core/src/sk/stuba/fiit/perconik/core/resources/Resources.java
@@ -6,40 +6,62 @@ import com.google.common.base.Preconditions;
 
 public final class Resources
 {
-	private static ResourceService service;
+	static final ServiceAccessor service = ServiceAccessor.INSTANCE;
 	
 	private Resources()
 	{
 		throw new AssertionError();
 	}
+
+	private static enum ServiceAccessor
+	{
+		INSTANCE;
+		
+		private ResourceService service;
+		
+		final synchronized void set(final ResourceService service)
+		{
+			this.service = Preconditions.checkNotNull(service);
+		}
+		
+		final synchronized ResourceService get()
+		{
+			if (this.service != null)
+			{
+				return this.service;
+			}
+			
+			return DefaultResources.getDefaultResourceService();
+		}
+	}
 	
 	public static final void setResourceService(final ResourceService service)
 	{
-		Resources.service = Preconditions.checkNotNull(service);
+		Resources.service.set(service);
 	}
 	
 	public static final ResourceService getResourceService()
 	{
-		return Resources.service;
+		return Resources.service.get();
 	}
 	
 	public static final <T extends Listener> void register(Class<T> type, Resource<T> resource)
 	{
-		service.register(type, resource);
+		getResourceService().register(type, resource);
 	}
 
 	public static final <T extends Listener> void unregister(Class<T> type, Resource<T> resource)
 	{
-		service.unregister(type, resource);
+		service.get().unregister(type, resource);
 	}
 
 	public static final Set<Resource<?>> registered()
 	{
-		return service.registered();
+		return service.get().registered();
 	}
 
 	public static final <T extends Listener> Set<Resource<? extends T>> forListener(Class<T> type)
 	{
-		return service.forListener(type);
+		return service.get().forListener(type);
 	}
 }
