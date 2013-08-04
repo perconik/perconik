78d0e39fc05a507efa15b02e0ac462a75034f4c8
Pavol Zbell <pavol.zbell@gmail.com>
2013-08-04 21:52:51 +0200

    Fix incorrect class names

---
+++ sk.stuba.fiit.perconik.core/src/sk/stuba/fiit/perconik/core/listeners/AbstractJavaElementChangeListener.java
@@ -0,0 +1,18 @@
+package sk.stuba.fiit.perconik.core.listeners;
+
+import java.util.EnumSet;
+import java.util.Set;
+import sk.stuba.fiit.perconik.eclipse.jdt.core.JavaElementChangeEventType;
+
+public abstract class AbstractJavaElementChangeListener extends AbstractFilteringListener<JavaElementChangeEventType> implements JavaElementChangeListener
+{
+	protected AbstractJavaElementChangeListener()
+	{
+		super(EnumSet.allOf(JavaElementChangeEventType.class));
+	}
+	
+	protected AbstractJavaElementChangeListener(final Set<JavaElementChangeEventType> types)
+	{
+		super(types);
+	}
+}
