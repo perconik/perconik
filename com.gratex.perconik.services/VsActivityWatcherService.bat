0b127088f32c1c570f4051f13246086f456f910c
Pavol Zbell <pavol.zbell@gmail.com>
2013-08-05 11:35:52 +0200

    Add activity service generation script

---
+++ com.gratex.perconik.services/VsActivityWatcherService.bat
@@ -0,0 +1,1 @@
+..\..\metro\bin\wsimport -extension -keep -Xnocompile -p com.gratex.perconik.services.activity -d src -b VsActivityWatcherService.xml http://localhost:7979/VsActivityWatcherService?wsdl
