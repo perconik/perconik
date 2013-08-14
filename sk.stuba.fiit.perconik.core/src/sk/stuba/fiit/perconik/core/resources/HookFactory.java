04dc2a13cb1accc1f2dd0bfca94f94d5abc9f883
Pavol Zbell <pavol.zbell@gmail.com>
2013-08-14 11:11:59 +0200

    Hide HookFactory from public API

--- sk.stuba.fiit.perconik.core/src/sk/stuba/fiit/perconik/core/resources/HookFactory.java
+++ sk.stuba.fiit.perconik.core/src/sk/stuba/fiit/perconik/core/resources/HookFactory.java
@@ -2,7 +2,7 @@ package sk.stuba.fiit.perconik.core.resources;
 
 import sk.stuba.fiit.perconik.core.Listener;
 
-public interface HookFactory<T, L extends Listener>
+interface HookFactory<T, L extends Listener>
 {
 	public Hook<T, L> create(L listener);
 }
