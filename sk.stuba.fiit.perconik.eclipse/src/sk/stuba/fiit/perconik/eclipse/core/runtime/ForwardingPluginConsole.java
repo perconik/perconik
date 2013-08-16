4ba32ecd485ad67e9b24fc51b3aebc94e2aba295
Pavol Zbell <pavol.zbell@gmail.com>
2013-08-16 10:29:39 +0200

    Add ForwardingPluginConsole

---
+++ sk.stuba.fiit.perconik.eclipse/src/sk/stuba/fiit/perconik/eclipse/core/runtime/ForwardingPluginConsole.java
@@ -0,0 +1,58 @@
+package sk.stuba.fiit.perconik.eclipse.core.runtime;
+
+import com.google.common.collect.ForwardingObject;
+
+public abstract class ForwardingPluginConsole extends ForwardingObject
+{
+	protected ForwardingPluginConsole()
+	{
+	}
+	
+	@Override
+	protected abstract PluginConsole delegate();
+	
+	public void put(final String message)
+	{
+		this.delegate().put(message);
+	}
+
+	public void put(final String format, final Object ... args)
+	{
+		this.delegate().put(format, args);
+	}
+
+	public void print(final String message)
+	{
+		this.delegate().print(message);
+	}
+
+	public void print(final String format, final Object ... args)
+	{
+		this.delegate().print(format, args);
+	}
+	
+	public void notice(final String message)
+	{
+		this.delegate().notice(message);
+	}
+
+	public void notice(final String format, Object ... args)
+	{
+		this.delegate().notice(format, args);
+	}
+
+	public void warning(final String message)
+	{
+		this.delegate().warning(message);
+	}
+
+	public void warning(final String format, Object ... args)
+	{
+		this.delegate().warning(format, args);
+	}
+
+	public void error(final String message, final Exception e)
+	{
+		this.delegate().error(message, e);
+	}
+}
