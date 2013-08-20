267b3662b1de9690d07a41448a18377de8ebb4fc
Pavol Zbell <pavol.zbell@gmail.com>
2013-08-20 14:04:27 +0200

    Unsupport command change handler for now

--- sk.stuba.fiit.perconik.core/src/sk/stuba/fiit/perconik/core/resources/CommandManagerChangeHandler.java
+++ sk.stuba.fiit.perconik.core/src/sk/stuba/fiit/perconik/core/resources/CommandManagerChangeHandler.java
@@ -1,6 +1,5 @@
 package sk.stuba.fiit.perconik.core.resources;
 
-import org.eclipse.swt.widgets.Display;
 import sk.stuba.fiit.perconik.core.listeners.CommandManagerChangeListener;
 
 enum CommandManagerChangeHandler implements Handler<CommandManagerChangeListener>
@@ -9,29 +8,22 @@ enum CommandManagerChangeHandler implements Handler<CommandManagerChangeListener
 	
 	public final void register(final CommandManagerChangeListener listener)
 	{
-		final Runnable addListener = new Runnable()
-		{
-			@Override
-			public final void run()
-			{
-				// TODO
-			}
-		};
-	
-		Display.getDefault().syncExec(addListener);
+		throw new UnsupportedOperationException("Not implemented yet");
+		
+//		final Runnable addListener = new Runnable()
+//		{
+//			@Override
+//			public final void run()
+//			{
+//				// TODO
+//			}
+//		};
+//	
+//		Display.getDefault().asyncExec(addListener);
 	}
 
 	public final void unregister(final CommandManagerChangeListener listener)
 	{
-		final Runnable removeListener = new Runnable()
-		{
-			@Override
-			public final void run()
-			{
-				// TODO
-			}
-		};
-	
-		Display.getDefault().syncExec(removeListener);
+		throw new UnsupportedOperationException("Not implemented yet");
 	}
 }
