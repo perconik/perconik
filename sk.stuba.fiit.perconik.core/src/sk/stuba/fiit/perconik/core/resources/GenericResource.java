46c3579d1a42fb49874d593d2a6caedf3d7c6f71
Pavol Zbell <pavol.zbell@gmail.com>
2013-08-21 17:06:25 +0200

    Implement name based equals and hashCode for standard resources

--- sk.stuba.fiit.perconik.core/src/sk/stuba/fiit/perconik/core/resources/GenericResource.java
+++ sk.stuba.fiit.perconik.core/src/sk/stuba/fiit/perconik/core/resources/GenericResource.java
@@ -1,12 +1,35 @@
 package sk.stuba.fiit.perconik.core.resources;
 
 import sk.stuba.fiit.perconik.core.Listener;
+import sk.stuba.fiit.perconik.core.Resource;
 
 final class GenericResource<L extends Listener> extends AbstractResource<L>
 {
 	GenericResource(final Pool<L> pool)
 	{
 		super(pool);
+	}
+
+	@Override
+	public final boolean equals(final Object o)
+	{
+		if (this == o)
+		{
+			return true;
+		}
+		
+		if (!(o instanceof Resource))
+		{
+			return false;
+		}
+		
+		return this.getName().equals(((Resource<?>) o).getName());
+	}
+
+	@Override
+	public final int hashCode()
+	{
+		return this.getName().hashCode();
 	}
 
 	@Override
