cccd854f588365ca8636ce68e9ff3faa7bb622ee
Pavol Zbell <pavol.zbell@gmail.com>
2013-08-25 14:46:30 +0200

    Add safe handlers

--- sk.stuba.fiit.perconik.core/src/sk/stuba/fiit/perconik/core/resources/Handlers.java
+++ sk.stuba.fiit.perconik.core/src/sk/stuba/fiit/perconik/core/resources/Handlers.java
@@ -3,6 +3,7 @@ package sk.stuba.fiit.perconik.core.resources;
 import sk.stuba.fiit.perconik.core.FilteringListener;
 import sk.stuba.fiit.perconik.utilities.IntegralConstant;
 import sk.stuba.fiit.perconik.utilities.IntegralConstantSupport;
+import com.google.common.base.Preconditions;
 
 final class Handlers
 {
@@ -11,8 +12,47 @@ final class Handlers
 		throw new AssertionError();
 	}
 	
+	private static final class SafeHandler<T> implements Handler<T>
+	{
+		private final Handler<T> handler;
+		
+		private final Class<T> type;
+		
+		public SafeHandler(final Handler<T> handler, final Class<T> type)
+		{
+			this.handler = Preconditions.checkNotNull(handler);
+			this.type    = Preconditions.checkNotNull(type);
+		}
+
+		private final T check(final T object)
+		{
+			return this.type.cast(Preconditions.checkNotNull(object));
+		}
+		
+		public final void register(final T object)
+		{
+			this.handler.register(check(object));
+		}
+
+		public final void unregister(final T object)
+		{
+			this.handler.unregister(check(object));
+		}
+
+		@Override
+		public final String toString()
+		{
+			return this.handler.toString();
+		}
+	}
+	
 	static final <E extends Enum<E> & IntegralConstant> int mask(final FilteringListener<E> listener)
 	{
 		return IntegralConstantSupport.constantsAsInteger(listener.getEventTypes());
+	}
+	
+	static final <T> Handler<T> safe(final Handler<T> handler, final Class<T> type)
+	{
+		return new SafeHandler<>(handler, type);
 	}
 }
