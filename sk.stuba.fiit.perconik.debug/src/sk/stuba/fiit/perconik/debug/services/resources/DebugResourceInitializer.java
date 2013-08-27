7c5ba8d18404c41663091d42ab3dbd3680f7a120
Pavol Zbell <pavol.zbell@gmail.com>
2013-08-27 18:00:35 +0200

    Add debug initializers and corresponding proxies

---
+++ sk.stuba.fiit.perconik.debug/src/sk/stuba/fiit/perconik/debug/services/resources/DebugResourceInitializer.java
@@ -0,0 +1,8 @@
+package sk.stuba.fiit.perconik.debug.services.resources;
+
+import sk.stuba.fiit.perconik.core.services.resources.ResourceInitializer;
+import sk.stuba.fiit.perconik.debug.services.DebugInitializer;
+
+public interface DebugResourceInitializer extends DebugInitializer, ResourceInitializer
+{
+}
---
+++ sk.stuba.fiit.perconik.debug/src/sk/stuba/fiit/perconik/debug/services/resources/DebugResourceInitializerProxy.java
@@ -0,0 +1,60 @@
+package sk.stuba.fiit.perconik.debug.services.resources;
+
+import sk.stuba.fiit.perconik.core.services.resources.ResourceInitializer;
+import sk.stuba.fiit.perconik.debug.Debug;
+import sk.stuba.fiit.perconik.debug.DebugObjectProxy;
+import sk.stuba.fiit.perconik.debug.runtime.DebugConsole;
+import com.google.common.base.Preconditions;
+
+public final class DebugResourceInitializerProxy extends DebugObjectProxy implements DebugResourceInitializer
+{
+	private final ResourceInitializer initializer;
+	
+	private DebugResourceInitializerProxy(final ResourceInitializer initializer, final DebugConsole console)
+	{
+		super(console);
+		
+		this.initializer = Preconditions.checkNotNull(initializer);
+	}
+	
+	public static final DebugResourceInitializerProxy wrap(final ResourceInitializer initializer)
+	{
+		return wrap(initializer, Debug.getDefaultConsole());
+	}
+
+	public static final DebugResourceInitializerProxy wrap(final ResourceInitializer initializer, final DebugConsole console)
+	{
+		if (initializer instanceof DebugResourceInitializerProxy)
+		{
+			return (DebugResourceInitializerProxy) initializer;
+		}
+		
+		return new DebugResourceInitializerProxy(initializer, console);
+	}
+	
+	public static final ResourceInitializer unwrap(final ResourceInitializer initializer)
+	{
+		if (initializer instanceof DebugResourceInitializerProxy)
+		{
+			return ((DebugResourceInitializerProxy) initializer).delegate();
+		}
+		
+		return initializer;
+	}
+
+	@Override
+	protected final ResourceInitializer delegate()
+	{
+		return this.initializer;
+	}
+
+	public final void run()
+	{
+		this.print("Registering resources:");
+		this.tab();
+		
+		this.delegate().run();
+
+		this.untab();
+	}
+}
