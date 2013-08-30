15879af9eb4f294a23b4044d15055c826df87963
Pavol Zbell <pavol.zbell@gmail.com>
2013-08-30 22:02:20 +0200

    Add service initial registration suppliers

---
+++ sk.stuba.fiit.perconik.core/src/sk/stuba/fiit/perconik/core/services/listeners/ListenerTypesSupplier.java
@@ -0,0 +1,9 @@
+package sk.stuba.fiit.perconik.core.services.listeners;
+
+import java.util.Set;
+import sk.stuba.fiit.perconik.core.Listener;
+import com.google.common.base.Supplier;
+
+public interface ListenerTypesSupplier extends Supplier<Set<Class<? extends Listener>>>
+{
+}
