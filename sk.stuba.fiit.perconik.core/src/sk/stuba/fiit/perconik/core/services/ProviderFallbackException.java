8542ba5ae2b7ea8a4a8146dd6fabef1023f9abd9
Pavol Zbell <pavol.zbell@gmail.com>
2013-09-06 00:54:42 +0200

    Add FallbackException

--- sk.stuba.fiit.perconik.core/src/sk/stuba/fiit/perconik/core/services/ProviderFallbackException.java
+++ sk.stuba.fiit.perconik.core/src/sk/stuba/fiit/perconik/core/services/ProviderFallbackException.java
@@ -1,20 +1,13 @@
 package sk.stuba.fiit.perconik.core.services;
 
-public class ProviderFallbackException extends RuntimeException
+import sk.stuba.fiit.perconik.utilities.FallbackException;
+
+public class ProviderFallbackException extends FallbackException
 {
 	private static final long serialVersionUID = 4422720325649677707L;
 
-	private final Throwable parent;
-	
 	public ProviderFallbackException(Throwable cause, Throwable parent)
 	{
-		super(cause);
-		
-		this.parent = parent;
-	}
-
-	public final Throwable getParent()
-	{
-		return this.parent;
+		super(parent, cause);
 	}
 }
