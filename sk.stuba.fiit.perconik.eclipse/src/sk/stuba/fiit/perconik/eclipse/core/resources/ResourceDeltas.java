a773cf4490e31f4f2fc3eeeb7c186d2469a19a44
Pavol Zbell <pavol.zbell@gmail.com>
2013-09-19 18:09:35 +0200

    Remove potentially dangerous helpers from ResourceDeltas

--- sk.stuba.fiit.perconik.eclipse/src/sk/stuba/fiit/perconik/eclipse/core/resources/ResourceDeltas.java
+++ sk.stuba.fiit.perconik.eclipse/src/sk/stuba/fiit/perconik/eclipse/core/resources/ResourceDeltas.java
@@ -1,7 +1,6 @@
 package sk.stuba.fiit.perconik.eclipse.core.resources;
 
 import java.util.Set;
-import org.eclipse.core.resources.IResourceChangeEvent;
 import org.eclipse.core.resources.IResourceDelta;
 import org.eclipse.core.resources.IResourceDeltaVisitor;
 import org.eclipse.core.runtime.CoreException;
@@ -18,16 +17,6 @@ public final class ResourceDeltas
 	private ResourceDeltas()
 	{
 		throw new AssertionError();
-	}
-	
-	public static final void accept(final IResourceChangeEvent event, final IResourceDeltaVisitor visitor)
-	{
-		accept(event.getDelta(), visitor);
-	}
-	
-	public static final void accept(final IResourceChangeEvent event, final IResourceDeltaVisitor visitor, final Set<ResourceMemberFlag> flags)
-	{
-		accept(event.getDelta(), visitor, flags);
 	}
 	
 	public static final void accept(final IResourceDelta delta, final IResourceDeltaVisitor visitor)
