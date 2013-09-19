c7b936d65ab11c271fafce65c6733190ed294409
Pavol Zbell <pavol.zbell@gmail.com>
2013-09-20 00:24:45 +0200

    Update Guava library to version 15.0

--- sk.stuba.fiit.perconik.core/src/sk/stuba/fiit/perconik/core/services/ServiceListener.java
+++ sk.stuba.fiit.perconik.core/src/sk/stuba/fiit/perconik/core/services/ServiceListener.java
@@ -2,11 +2,17 @@ package sk.stuba.fiit.perconik.core.services;
 
 /**
  * A mirror of {@code com.google.common.util.concurrent.Service.Listener}.
- * Use when the mirrored interface name clashes with core {@code Listener}. 
+ * Use when the mirrored class name clashes with core {@code Listener}. 
  * 
  * @author Pavol Zbell
  * @since 1.0
  */
-public interface ServiceListener extends Service.Listener
+public class ServiceListener extends Service.Listener
 {
+	/**
+	 * Constructor for use by subclasses.
+	 */
+	protected ServiceListener()
+	{
+	}
 }
