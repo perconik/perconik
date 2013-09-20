cdf356c0ff91ab13e42e01e19e3d578c98b8fb21
Pavol Zbell <pavol.zbell@gmail.com>
2013-09-20 15:22:18 +0200

    Add more collection related helper methods

--- sk.stuba.fiit.perconik.utilities/src/sk/stuba/fiit/perconik/utilities/MoreSets.java
+++ sk.stuba.fiit.perconik.utilities/src/sk/stuba/fiit/perconik/utilities/MoreSets.java
@@ -1,7 +1,11 @@
 package sk.stuba.fiit.perconik.utilities;
 
+import java.util.EnumSet;
 import java.util.HashSet;
 import java.util.Iterator;
+import java.util.LinkedHashSet;
+import java.util.Set;
+import java.util.TreeSet;
 import com.google.common.collect.Iterators;
 import com.google.common.collect.Sets;
 
@@ -18,7 +22,7 @@ public final class MoreSets
 		throw new AssertionError();
 	}
 	
-	public static <E> HashSet<E> newHashSet(Iterable<? extends E> a, Iterable<? extends E> b)
+	public static final <E> HashSet<E> newHashSet(Iterable<? extends E> a, Iterable<? extends E> b)
 	{
 		HashSet<E> set = Sets.newHashSet(a);
 		
@@ -27,7 +31,7 @@ public final class MoreSets
 		return set;
 	}
 
-	public static <E> HashSet<E> newHashSet(Iterator<? extends E> a, Iterator<? extends E> b)
+	public static final <E> HashSet<E> newHashSet(Iterator<? extends E> a, Iterator<? extends E> b)
 	{
 		HashSet<E> set = Sets.newHashSet();
 
@@ -35,5 +39,35 @@ public final class MoreSets
 		Iterators.addAll(set, b);
 		
 		return set;
+	}
+	
+	public static final <E extends Enum<E>> EnumSet<E> newEnumSet(Iterable<E> elements)
+	{
+		return EnumSet.copyOf(MoreLists.toList(elements));
+	}
+	
+	public static final <E> Set<E> toSet(Iterable<E> elements)
+	{
+		return elements instanceof Set ? (Set<E>) elements : Sets.newHashSet(elements);
+	}
+	
+	public static final <E> HashSet<E> toHashSet(Iterable<E> elements)
+	{
+		return elements instanceof HashSet ? (HashSet<E>) elements : Sets.newHashSet(elements);
+	}
+	
+	public static final <E> LinkedHashSet<E> toLinkedHashSet(Iterable<E> elements)
+	{
+		return elements instanceof LinkedHashSet ? (LinkedHashSet<E>) elements : Sets.newLinkedHashSet(elements);
+	}
+	
+	public static final <E extends Comparable<E>> TreeSet<E> toTreeSet(Iterable<E> elements)
+	{
+		return elements instanceof TreeSet ? (TreeSet<E>) elements : Sets.newTreeSet(elements);
+	}
+	
+	public static final <E extends Enum<E>> EnumSet<E> toEnumSet(Iterable<E> elements)
+	{
+		return elements instanceof EnumSet ? (EnumSet<E>) elements : newEnumSet(elements);
 	}
 }
