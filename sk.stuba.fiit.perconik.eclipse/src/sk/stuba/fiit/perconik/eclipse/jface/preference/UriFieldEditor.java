4da99969c3de1f20c8b99848c30fb732259ccfac
Pavol Zbell <pavol.zbell@gmail.com>
2013-09-30 09:40:50 +0200

    Add URI and URL field editors

---
+++ sk.stuba.fiit.perconik.eclipse/src/sk/stuba/fiit/perconik/eclipse/jface/preference/UriFieldEditor.java
@@ -0,0 +1,42 @@
+package sk.stuba.fiit.perconik.eclipse.jface.preference;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+import org.eclipse.jface.preference.StringFieldEditor;
+import org.eclipse.swt.widgets.Composite;
+
+public class UriFieldEditor extends StringFieldEditor
+{
+	public UriFieldEditor(String name, String label, Composite parent)
+	{
+		super(name, label, parent);
+	}
+	
+	public UriFieldEditor(String name, String label, int width, Composite parent)
+	{
+		super(name, label, width, parent);
+	}
+
+	public UriFieldEditor(String name, String label, int width, int strategy, Composite parent)
+	{
+		super(name, label, width, strategy, parent);
+	}
+
+	@Override
+	protected boolean doCheckState()
+	{
+		return this.getUriValue() != null;
+	}
+	
+	public URI getUriValue()
+	{
+		try
+		{
+			return new URI(this.getStringValue());
+		}
+		catch (URISyntaxException e)
+		{
+			return null;
+		}
+	}
+}
---
+++ sk.stuba.fiit.perconik.eclipse/src/sk/stuba/fiit/perconik/eclipse/jface/preference/UrlFieldEditor.java
@@ -0,0 +1,42 @@
+package sk.stuba.fiit.perconik.eclipse.jface.preference;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import org.eclipse.jface.preference.StringFieldEditor;
+import org.eclipse.swt.widgets.Composite;
+
+public class UrlFieldEditor extends StringFieldEditor
+{
+	public UrlFieldEditor(String name, String label, Composite parent)
+	{
+		super(name, label, parent);
+	}
+	
+	public UrlFieldEditor(String name, String label, int width, Composite parent)
+	{
+		super(name, label, width, parent);
+	}
+
+	public UrlFieldEditor(String name, String label, int width, int strategy, Composite parent)
+	{
+		super(name, label, width, strategy, parent);
+	}
+
+	@Override
+	protected boolean doCheckState()
+	{
+		return this.getUrlValue() != null;
+	}
+	
+	public URL getUrlValue()
+	{
+		try
+		{
+			return new URL(this.getStringValue());
+		}
+		catch (MalformedURLException e)
+		{
+			return null;
+		}
+	}
+}
