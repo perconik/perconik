6dc22d175455aaaa0ffa05d9c3a7a8693e0e5e46
Pavol Zbell <pavol.zbell@gmail.com>
2013-10-14 19:11:28 +0200

    Add helpers for class files

---
+++ sk.stuba.fiit.perconik.core.java/src/sk/stuba/fiit/perconik/core/java/ClassFiles.java
@@ -0,0 +1,41 @@
+package sk.stuba.fiit.perconik.core.java;
+
+import java.util.LinkedList;
+import org.eclipse.jdt.core.IClassFile;
+import org.eclipse.jdt.core.IJavaElement;
+import sk.stuba.fiit.perconik.eclipse.jdt.core.JavaElementType;
+import com.google.common.base.Joiner;
+import com.google.common.collect.Lists;
+
+public final class ClassFiles
+{
+	private ClassFiles()
+	{
+		throw new AssertionError();
+	}
+	
+	public static final String path(final IClassFile file)
+	{
+		LinkedList<String> segments = Lists.newLinkedList();
+		
+		for (IJavaElement element: JavaElements.upToRoot(file))
+		{
+			switch (JavaElementType.valueOf(element))
+			{
+				case CLASS_FILE:
+				case PACKAGE_FRAGMENT:
+					segments.addFirst(element.getElementName());
+					break;
+					
+				case PACKAGE_FRAGMENT_ROOT:
+					segments.addFirst(element.getPath().toString());
+					break;
+
+				default:
+					continue;
+			}
+		}
+		
+		return Joiner.on('/').join(segments);
+	}
+}
