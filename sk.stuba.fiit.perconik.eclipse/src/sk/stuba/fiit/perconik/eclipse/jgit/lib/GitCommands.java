11b2f531a5e1328529600372f0c7c409e7e5f206
Pavol Zbell <pavol.zbell@gmail.com>
2013-10-22 12:32:55 +0200

    Search for Git repositories from project location down to root

--- sk.stuba.fiit.perconik.eclipse/src/sk/stuba/fiit/perconik/eclipse/jgit/lib/GitCommands.java
+++ sk.stuba.fiit.perconik.eclipse/src/sk/stuba/fiit/perconik/eclipse/jgit/lib/GitCommands.java
@@ -1,6 +1,7 @@
 package sk.stuba.fiit.perconik.eclipse.jgit.lib;
 
 import org.eclipse.core.resources.IProject;
+import org.eclipse.core.runtime.IPath;
 import org.eclipse.jgit.api.Git;
 import org.eclipse.jgit.errors.RepositoryNotFoundException;
 import com.google.common.base.Throwables;
@@ -16,10 +17,27 @@ public final class GitCommands
 	{
 		try
 		{
-			return Git.open(project.getLocation().toFile());
-		}
-		catch (RepositoryNotFoundException e)
-		{
+			IPath path = project.getLocation();
+
+			while (true)
+			{
+				try
+				{
+					return Git.open(path.toFile());
+				}
+				catch (RepositoryNotFoundException e)
+				{
+					if (path.segmentCount() == 0)
+					{
+						break;
+					}
+					
+					path = path.removeLastSegments(1);
+					
+					continue;
+				}
+			}
+			
 			return null;
 		}
 		catch (Exception e)
