2ee35da0fb175bca6090f614199209ddfb5403d6
Pavol Zbell <pavol.zbell@gmail.com>
2013-11-26 11:14:53 +0100

    Refactor activatable handler to support multiple activations

--- sk.stuba.fiit.perconik.eclipse/src/sk/stuba/fiit/perconik/eclipse/core/commands/ActivatableHandler.java
+++ sk.stuba.fiit.perconik.eclipse/src/sk/stuba/fiit/perconik/eclipse/core/commands/ActivatableHandler.java
@@ -2,18 +2,21 @@ package sk.stuba.fiit.perconik.eclipse.core.commands;
 
 import static com.google.common.base.Preconditions.checkNotNull;
 import static com.google.common.base.Preconditions.checkState;
+import java.util.Collection;
+import java.util.Map;
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.ui.handlers.IHandlerActivation;
 import org.eclipse.ui.handlers.IHandlerService;
-import com.google.common.base.Optional;
+import com.google.common.collect.Lists;
+import com.google.common.collect.Maps;
 
 public abstract class ActivatableHandler extends AbstractHandler
 {
-	private Optional<IHandlerActivation> activation;
+	private Map<String, IHandlerActivation> activations;
 	
 	protected ActivatableHandler()
 	{
-		this.activation = Optional.absent();
+		this.activations = Maps.newLinkedHashMap();
 	}
 	
 	public final IHandlerActivation activate(final String identifier)
@@ -25,32 +28,58 @@ public abstract class ActivatableHandler extends AbstractHandler
 	{
 		checkNotNull(service);
 
-		synchronized (this.activation)
+		synchronized (this.activations)
 		{
-			checkState(!this.activation.isPresent());
-			
-			this.activation = Optional.of(service.activateHandler(identifier, this));
-			
-			return this.activation.get();
+			return this.activateInternal(identifier, service);
 		}
 	}
 	
+	private final IHandlerActivation activateInternal(final String identifier, final IHandlerService service)
+	{
+		checkState(!this.activations.containsKey(identifier));
+		
+		IHandlerActivation activation = service.activateHandler(identifier, this);
+		
+		this.activations.put(identifier, activation);
+		
+		return activation;
+	}
+
 	public final void deactivate()
 	{
-		synchronized (this.activation)
+		synchronized (this.activations)
 		{
-			IHandlerActivation activation = this.activation.get();
-			IHandlerService    service    = activation.getHandlerService();
-
-			if (service != null)
+			for (String identifier: this.activations.keySet())
 			{
-				service.deactivateHandler(activation);
+				this.deactivateInternal(identifier);
 			}
 		}
 	}
 
-	public final Optional<IHandlerActivation> getActivation()
+	public final void deactivate(final String identifier)
 	{
-		return this.activation;
+		synchronized (this.activations)
+		{
+			this.deactivateInternal(identifier);
+		}
+	}
+	
+	private final void deactivateInternal(final String identifier)
+	{
+		IHandlerActivation activation = this.activations.get(identifier);
+		
+		checkState(identifier != null);
+		
+		IHandlerService service = activation.getHandlerService();
+
+		if (service != null)
+		{
+			service.deactivateHandler(activation);
+		}
+	}
+
+	public final Collection<IHandlerActivation> getActivations()
+	{
+		return Lists.newArrayList(this.activations.values());
 	}
 }
