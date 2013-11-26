c33bacbfe430ccc63d34125a048e2a43b3d546a5
Pavol Zbell <pavol.zbell@gmail.com>
2013-11-26 11:02:21 +0100

    Refactor self activatable command handler

--- sk.stuba.fiit.perconik.eclipse/src/sk/stuba/fiit/perconik/eclipse/core/commands/ActivatableHandler.java
+++ sk.stuba.fiit.perconik.eclipse/src/sk/stuba/fiit/perconik/eclipse/core/commands/ActivatableHandler.java
@@ -2,7 +2,6 @@ package sk.stuba.fiit.perconik.eclipse.core.commands;
 
 import static com.google.common.base.Preconditions.checkNotNull;
 import static com.google.common.base.Preconditions.checkState;
-import javax.annotation.Nullable;
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.ui.handlers.IHandlerActivation;
 import org.eclipse.ui.handlers.IHandlerService;
@@ -10,11 +9,11 @@ import com.google.common.base.Optional;
 
 public abstract class ActivatableHandler extends AbstractHandler
 {
-	@Nullable
-	private IHandlerActivation activation;
+	private Optional<IHandlerActivation> activation;
 	
 	protected ActivatableHandler()
 	{
+		this.activation = Optional.absent();
 	}
 	
 	public final IHandlerActivation activate(final String identifier)
@@ -28,9 +27,11 @@ public abstract class ActivatableHandler extends AbstractHandler
 
 		synchronized (this.activation)
 		{
-			checkState(this.activation == null);
+			checkState(!this.activation.isPresent());
 			
-			return this.activation = service.activateHandler(identifier, this);
+			this.activation = Optional.of(service.activateHandler(identifier, this));
+			
+			return this.activation.get();
 		}
 	}
 	
@@ -38,14 +39,18 @@ public abstract class ActivatableHandler extends AbstractHandler
 	{
 		synchronized (this.activation)
 		{
-			checkState(this.activation != null);
+			IHandlerActivation activation = this.activation.get();
+			IHandlerService    service    = activation.getHandlerService();
 
-			this.activation.getHandlerService().deactivateHandler(this.activation);
+			if (service != null)
+			{
+				service.deactivateHandler(activation);
+			}
 		}
 	}
 
 	public final Optional<IHandlerActivation> getActivation()
 	{
-		return Optional.fromNullable(this.activation);
+		return this.activation;
 	}
 }
