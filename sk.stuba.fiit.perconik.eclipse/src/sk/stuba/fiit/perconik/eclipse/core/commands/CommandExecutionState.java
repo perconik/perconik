4a7c561886a655643bca53f7317cd188af091d20
Pavol Zbell <pavol.zbell@gmail.com>
2013-11-26 13:26:18 +0100

    Add command execution states

---
+++ sk.stuba.fiit.perconik.eclipse/src/sk/stuba/fiit/perconik/eclipse/core/commands/CommandExecutionState.java
@@ -0,0 +1,18 @@
+package sk.stuba.fiit.perconik.eclipse.core.commands;
+
+public enum CommandExecutionState
+{
+	UNDEFINED,
+	
+	DISABLED,
+	
+	UNHANDLED,
+	
+	WAITING,
+	
+	EXECUTING,
+	
+	SUCCEEDED,
+	
+	FAILED;
+}
