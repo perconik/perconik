cbd4c17e9854656bae4f0212499329113a26e07c
Pavol Zbell <pavol.zbell@gmail.com>
2013-12-26 18:03:53 +0100

    Add package info for examples

---
+++ sk.stuba.fiit.perconik.core.java.examples/src/sk/stuba/fiit/perconik/core/java/examples/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * Examples of Java Model core extensions usage.
+ */
+@ParametersAreNonnullByDefault
+package sk.stuba.fiit.perconik.core.java.examples;
+
+import javax.annotation.ParametersAreNonnullByDefault;
