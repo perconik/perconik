2dd1119cea0866e206a115f4ae8dd140ac58cfdc
Pavol Zbell <pavol.zbell@gmail.com>
2014-01-03 19:30:38 +0100

    Implement search result listener handler

--- sk.stuba.fiit.perconik.core/src/sk/stuba/fiit/perconik/core/resources/SearchResultHandler.java
+++ sk.stuba.fiit.perconik.core/src/sk/stuba/fiit/perconik/core/resources/SearchResultHandler.java
@@ -1,16 +1,21 @@
 package sk.stuba.fiit.perconik.core.resources;
 
 import sk.stuba.fiit.perconik.core.listeners.SearchResultListener;
+import sk.stuba.fiit.perconik.core.resources.SearchResultHook.Support;
 
 enum SearchResultHandler implements Handler<SearchResultListener>
 {
 	INSTANCE;
 	
+	private final Support support = new Support();
+	
 	public final void register(final SearchResultListener listener)
 	{
+		this.support.hook(DefaultResources.getSearchQueryResource(), listener);
 	}
 
 	public final void unregister(final SearchResultListener listener)
 	{
+		this.support.unhook(DefaultResources.getSearchQueryResource(), listener);
 	}
 }
