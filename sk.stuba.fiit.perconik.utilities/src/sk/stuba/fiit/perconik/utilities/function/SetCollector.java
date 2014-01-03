02490590cd066fcf3b49c96ed13d83933b899093
Pavol Zbell <pavol.zbell@gmail.com>
2014-01-03 21:24:52 +0100

    Format some code

--- sk.stuba.fiit.perconik.utilities/src/sk/stuba/fiit/perconik/utilities/function/SetCollector.java
+++ sk.stuba.fiit.perconik.utilities/src/sk/stuba/fiit/perconik/utilities/function/SetCollector.java
@@ -3,8 +3,8 @@ package sk.stuba.fiit.perconik.utilities.function;
 import java.util.Set;
 import javax.annotation.Nullable;
 
-public interface SetCollector<T, R> extends Collector<T, R>
+public interface SetCollector<T, E> extends Collector<T, E>
 {
 	@Override
-	public Set<R> apply(@Nullable T input);
+	public Set<E> apply(@Nullable T input);
 }
