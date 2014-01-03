949806724f179e90667b834107603bb5f59b38f2
Pavol Zbell <pavol.zbell@gmail.com>
2014-01-03 01:32:34 +0100

    Add missing Nullable annotation to NodeTypeDivider

--- sk.stuba.fiit.perconik.core.java/src/sk/stuba/fiit/perconik/core/java/dom/NodeTypeDivider.java
+++ sk.stuba.fiit.perconik.core.java/src/sk/stuba/fiit/perconik/core/java/dom/NodeTypeDivider.java
@@ -42,7 +42,7 @@ public final class NodeTypeDivider<N extends ASTNode> implements MultimapCollect
 			this.result = LinkedListMultimap.create(NodeType.count());
 		}
 		
-		final Multimap<NodeType, ASTNode> perform(final N node)
+		final Multimap<NodeType, ASTNode> perform(@Nullable final N node)
 		{
 			if (node == null)
 			{
