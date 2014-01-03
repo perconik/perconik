07e3c73157031b170f77310b34c10c92e1a51343
Pavol Zbell <pavol.zbell@gmail.com>
2014-01-03 18:38:26 +0100

    Add interfaces for search listeners

---
+++ sk.stuba.fiit.perconik.core/src/sk/stuba/fiit/perconik/core/listeners/SearchQueryListener.java
@@ -0,0 +1,17 @@
+package sk.stuba.fiit.perconik.core.listeners;
+
+import org.eclipse.search.ui.IQueryListener;
+import sk.stuba.fiit.perconik.core.Listener;
+
+/**
+ * A search query listener.
+ * 
+ * @see Listener
+ * @see IQueryListener
+ * 
+ * @author Pavol Zbell
+ * @since 1.0
+ */
+public interface SearchQueryListener extends Listener, IQueryListener
+{
+}
---
+++ sk.stuba.fiit.perconik.core/src/sk/stuba/fiit/perconik/core/listeners/SearchResultListener.java
@@ -0,0 +1,17 @@
+package sk.stuba.fiit.perconik.core.listeners;
+
+import org.eclipse.search.ui.ISearchResultListener;
+import sk.stuba.fiit.perconik.core.Listener;
+
+/**
+ * A search result listener.
+ * 
+ * @see Listener
+ * @see ISearchResultListener
+ * 
+ * @author Pavol Zbell
+ * @since 1.0
+ */
+public interface SearchResultListener extends Listener, ISearchResultListener
+{
+}
