79e6407dd49192de7443b32143a6390c713565ad
Pavol Zbell <pavol.zbell@gmail.com>
2014-01-11 03:14:43 +0100

    Fix annotation to string helper

--- sk.stuba.fiit.perconik.core/src/sk/stuba/fiit/perconik/core/annotations/Annotations.java
+++ sk.stuba.fiit.perconik.core/src/sk/stuba/fiit/perconik/core/annotations/Annotations.java
@@ -1,11 +1,15 @@
 package sk.stuba.fiit.perconik.core.annotations;
 
 import java.lang.annotation.Annotation;
+import java.util.Objects;
+import java.util.Set;
 import sk.stuba.fiit.perconik.utilities.Exceptional;
 import sk.stuba.fiit.perconik.utilities.MoreLists;
+import sk.stuba.fiit.perconik.utilities.SmartStringBuilder;
 import sk.stuba.fiit.perconik.utilities.reflect.accessor.Accessor;
 import sk.stuba.fiit.perconik.utilities.reflect.accessor.Accessors;
 import com.google.common.base.CaseFormat;
+import com.google.common.collect.Sets;
 
 /**
  * Static helper methods pertaining to the core annotations. 
@@ -22,7 +26,7 @@ public final class Annotations
 	
 	public static final String toString(final Annotation annotation)
 	{
-		StringBuilder builder = new StringBuilder();
+		SmartStringBuilder builder = new SmartStringBuilder();
 		
 		builder.append(CaseFormat.UPPER_CAMEL.to(CaseFormat.LOWER_HYPHEN, annotation.annotationType().getSimpleName()));
 		
@@ -30,16 +34,31 @@ public final class Annotations
 		
 		if (result.isSuccess())
 		{
-			builder.append(" (");
+			Set<String> elements = Sets.newTreeSet();
 			
-			for (Object value: MoreLists.wrap(result.get().get()))
+			for (Object element: MoreLists.wrap(result.get().get()))
 			{
-				builder.append(value instanceof Class ? ((Class<?>) value).getCanonicalName() : String.valueOf(value));
-				builder.append(", ");
+				if (element instanceof Class)
+				{
+					elements.add(((Class<?>) element).getCanonicalName());
+					
+					continue;
+				}
+
+				String value = Objects.toString(element, "");
+				
+				if (value.isEmpty())
+				{
+					continue;
+				}
+				
+				elements.add(value);
 			}
-			
-			builder.setLength(builder.length() - 2);
-			builder.append(")");
+
+			if (!elements.isEmpty())
+			{
+				builder.append(" (").list(elements, ", ").append(")");
+			}
 		}
 		
 		return builder.toString();
