d3abb124b2d18db022747e0498c9c7ecd4baced5
Pavol Zbell <pavol.zbell@gmail.com>
2014-02-02 18:26:41 +0100

    Document IdeCommitListener behavior and data

--- com.gratex.perconik.activity/src/com/gratex/perconik/activity/ide/listeners/IdeCommitListener.java
+++ com.gratex.perconik.activity/src/com/gratex/perconik/activity/ide/listeners/IdeCommitListener.java
@@ -25,7 +25,26 @@ import com.gratex.perconik.services.uaca.vs.IdeCheckinDto;
  * the <i>Activity Watcher Service</i> to be transferred into the
  * <i>User Activity Client Application</i> for further processing.
  * 
- * <p> TODO document how DTOs are build and what data they contain
+ * <p>Commit listener listens to Git commit events only.
+ * 
+ * <p>Data available in an {@code IdeCheckinDto}:
+ * 
+ * <ul>
+ *   <li>{@code idInRcs} - current Git commit identifier (40 hexadecimal
+ *   characters), for example {@code ffba951d35f710abee873db3f5547043aeb3fde9}.
+ *   <li>{@code rcsServer} - see {@code RcsServerDto} below.
+ *   <li>See {@link IdeListener} for documentation of inherited data.
+ * </ul>
+ * 
+ * <p>Data available in an {@code RcsServerDto}:
+ * 
+ * <ul>
+ *   <li>{@code path} - remote origin URL from the nearest Git repository,
+ *   for example {@code https://github.com/perconik/perconik.git}. The nearest
+ *   Git repository is the first one found on path starting in project root,
+ *   going through workspace root down to the file system root.
+ *   <li>{@code type} - always {@code "git"}.
+ * </ul>
  * 
  * @author Pavol Zbell
  * @since 1.0
