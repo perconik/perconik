e2c959229902a36204315200bd337d4464f2bb11
Pavol Zbell <pavol.zbell@gmail.com>
2014-03-02 22:34:15 +0100

    Refactor annotations resolving for registrables

--- sk.stuba.fiit.perconik.core/src/sk/stuba/fiit/perconik/core/Registrables.java
+++ sk.stuba.fiit.perconik.core/src/sk/stuba/fiit/perconik/core/Registrables.java
@@ -2,16 +2,24 @@ package sk.stuba.fiit.perconik.core;
 
 import java.io.Serializable;
 import java.lang.annotation.Annotation;
+import java.lang.annotation.IncompleteAnnotationException;
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
 import java.util.LinkedList;
+import java.util.Objects;
 import java.util.Set;
 import sk.stuba.fiit.perconik.core.annotations.Experimental;
 import sk.stuba.fiit.perconik.core.annotations.Internal;
 import sk.stuba.fiit.perconik.core.annotations.Persistent;
 import sk.stuba.fiit.perconik.core.annotations.Unsafe;
 import sk.stuba.fiit.perconik.core.annotations.Unsupported;
+import sk.stuba.fiit.perconik.core.annotations.Version;
+import sk.stuba.fiit.perconik.osgi.framework.Bundles;
 import sk.stuba.fiit.perconik.utilities.reflect.Reflections;
 import sk.stuba.fiit.perconik.utilities.reflect.annotation.Annotable;
 import sk.stuba.fiit.perconik.utilities.reflect.annotation.Annotations;
+import com.google.common.base.Strings;
 
 /**
  * Static helper methods pertaining to the core registrables. 
@@ -31,7 +39,63 @@ public final class Registrables
 	{
 	}
 	
+	private static final class VersionHandler implements InvocationHandler, Serializable
+	{
+		private static final long serialVersionUID = 0L;
+
+		private final String value;
+		
+		VersionHandler(final String value)
+		{
+			this.value = value;
+		}
+
+		public final Object invoke(final Object proxy, final Method method, final Object[] args) throws Throwable
+		{
+			String member = method.getName();
+			
+			Class<?>[] parameters = method.getParameterTypes();
+			
+			if (member.equals("equals") && parameters.length == 1 && parameters[0] == Object.class)
+			{
+				Object o = args[0];
+				
+				return (o == this) || (o instanceof Version && Objects.equals(this.value, ((Version) o).value())); 
+			}
+			
+	        switch (member)
+	        {
+	        	case "hashCode":
+	        		return (127 * "value".hashCode()) ^ Objects.hashCode(this.value);
+	        		
+	        	case "toString":
+	        		return "@" + Version.class.getName() +"(" + String.valueOf(this.value) + ")";
+	        		
+	        	case "annotationType":
+	        		return Version.class;
+	        		
+	        	case "value":
+	        		return this.value;
+	        		
+	        	default:
+	        		throw new IncompleteAnnotationException(Version.class, member);
+	        }
+		}
+	}
+	
 	private static final Persistent persistent = PersistentMark.class.getAnnotation(Persistent.class);
+	
+	private static final <R extends Registrable> Version version(final Class<R> type)
+	{
+		String value = Bundles.forClass(type).getVersion().toString();
+		
+		if (Strings.isNullOrEmpty(value))
+		{
+			throw new IllegalArgumentException();
+		}
+		
+		return (Version) Proxy.newProxyInstance(Version.class.getClassLoader(), new Class[] { Version.class }, new VersionHandler(value));
+	}
 	
 	public static final <R extends Registrable> Annotable toAnnotable(final Class<R> type)
 	{
@@ -45,8 +109,17 @@ public final class Registrables
 		{
 			annotations.add(persistent);
 		}
+				
+		Annotable annotable = Annotations.asAnnotable(annotations);
 		
-		return Annotations.asAnnotable(annotations); 
+		if (annotable.hasAnnotation(Version.class))
+		{
+			return annotable;
+		}
+		
+		annotations.add(version(type));
+		
+		return Annotations.asAnnotable(annotations);
 	}
 	
 	public static final boolean isExperimental(final Class<? extends Registrable> type)
