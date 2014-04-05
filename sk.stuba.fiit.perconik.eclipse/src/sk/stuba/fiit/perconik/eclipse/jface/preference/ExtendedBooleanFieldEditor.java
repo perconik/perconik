6941aeb4f4b628a6e5b9f5a6d5ae179395a98502
Pavol Zbell <pavol.zbell@gmail.com>
2014-04-05 12:13:46 +0200

    Add extended boolean field editor

---
+++ sk.stuba.fiit.perconik.eclipse/src/sk/stuba/fiit/perconik/eclipse/jface/preference/ExtendedBooleanFieldEditor.java
@@ -0,0 +1,42 @@
+
+package sk.stuba.fiit.perconik.eclipse.jface.preference;
+
+import org.eclipse.jface.preference.BooleanFieldEditor;
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.Composite;
+
+public class ExtendedBooleanFieldEditor extends BooleanFieldEditor
+{
+	private Button control;
+	
+	public ExtendedBooleanFieldEditor(String name, String label, Composite parent)
+	{
+		this(name, label, DEFAULT, parent);
+	}
+	
+	public ExtendedBooleanFieldEditor(String name, String label, int style, Composite parent)
+	{
+		super(name, label, style, parent);
+	}
+
+	public Button getChangeControl()
+	{
+		if (this.control == null)
+		{
+			throw new IllegalStateException();
+		}
+		
+		return this.getChangeControl(null);
+	}
+	
+	@Override
+	public Button getChangeControl(Composite parent)
+	{
+		if (this.control == null)
+		{
+			this.control = super.getChangeControl(parent);
+		}
+		
+		return this.control;
+	}
+}
