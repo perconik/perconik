208cba818b89d50f7b4c8d004e345ecda998a52f
pruttkay-nedecky <peter.ruttkay-nedecky@gratex.com>
2014-05-13 15:16:39 +0200

    -SelectionChange has been temporary disabled in IdeCodeListener due to bug that causes continuous event sequence during selection mouse move -Update site has been changed in feature xmls

--- com.gratex.perconik.activity/src/com/gratex/perconik/activity/ide/listeners/IdeCodeListener.java
+++ com.gratex.perconik.activity/src/com/gratex/perconik/activity/ide/listeners/IdeCodeListener.java
@@ -312,31 +312,32 @@ public final class IdeCodeListener extends IdeListener implements CommandExecuti
 		
 		UacaProxy.sendCodeEvent(build(time, resource, data), IdeCodeEventType.PASTE);
 	}
-
-	static final void process(final long time, final IWorkbenchPart part, final ITextSelection selection)
-	{
-		if (!(part instanceof IEditorPart))
-		{
-			return;
-		}
-		
-		IEditorPart editor   = (IEditorPart) part;
-		IDocument   document = Editors.getDocument(editor);
-		
-		UnderlyingDocument<?> resource = UnderlyingDocument.from(editor);
-		
-		if (document == null || resource == null)
-		{
-			return;
-		}
-
-		Region data = Region.of(document, selection.getOffset(), selection.getLength(), selection.getText());
-		
-		assert data.start.line == selection.getStartLine();
-		assert data.end.line   == selection.getEndLine();
-		
-		UacaProxy.sendCodeEvent(build(time, resource, data), IdeCodeEventType.SELECTION_CHANGED);
-	}
+	
+	//todo: add timer - fix continuous event sequence 
+//	static final void process(final long time, final IWorkbenchPart part, final ITextSelection selection)
+//	{
+//		if (!(part instanceof IEditorPart))
+//		{
+//			return;
+//		}
+//		
+//		IEditorPart editor   = (IEditorPart) part;
+//		IDocument   document = Editors.getDocument(editor);
+//		
+//		UnderlyingDocument<?> resource = UnderlyingDocument.from(editor);
+//		
+//		if (document == null || resource == null)
+//		{
+//			return;
+//		}
+//
+//		Region data = Region.of(document, selection.getOffset(), selection.getLength(), selection.getText());
+//		
+//		assert data.start.line == selection.getStartLine();
+//		assert data.end.line   == selection.getEndLine();
+//		
+//		UacaProxy.sendCodeEvent(build(time, resource, data), IdeCodeEventType.SELECTION_CHANGED);
+//	}
 	
 	public final void documentAboutToBeChanged(final DocumentEvent event)
 	{
@@ -366,15 +367,16 @@ public final class IdeCodeListener extends IdeListener implements CommandExecuti
 
 	public final void selectionChanged(final IWorkbenchPart part, final ITextSelection selection)
 	{
-		final long time = Utilities.currentTime();
-		
-		execute(new Runnable()
-		{
-			public final void run()
-			{
-				process(time, part, selection);
-			}
-		});
+		//todo: add timer - fix continuous event sequence 
+//		final long time = Utilities.currentTime();
+//		
+//		execute(new Runnable()
+//		{
+//			public final void run()
+//			{
+//				process(time, part, selection);
+//			}
+//		});
 	}
 
 	public final void preExecute(final String id, final ExecutionEvent event)
