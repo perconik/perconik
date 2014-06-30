3d9aaddfb68389527db96667b73bd0169000c806
Pavol Zbell <pavol.zbell@gmail.com>
2014-06-30 16:37:22 +0200

    Add MoreSuppliers helpers

---
+++ sk.stuba.fiit.perconik.utilities/src/sk/stuba/fiit/perconik/utilities/MoreSuppliers.java
@@ -0,0 +1,23 @@
+package sk.stuba.fiit.perconik.utilities;
+
+import java.util.concurrent.Callable;
+import com.google.common.base.Supplier;
+
+public final class MoreSuppliers
+{
+	private MoreSuppliers()
+	{
+		throw new AssertionError();
+	}
+	
+	public static final <V> Callable<V> asCallable(final Supplier<V> supplier)
+	{
+		return new Callable<V>()
+		{
+			public final V call() throws Exception
+			{
+				return supplier.get();
+			}
+		};
+	}
+}
