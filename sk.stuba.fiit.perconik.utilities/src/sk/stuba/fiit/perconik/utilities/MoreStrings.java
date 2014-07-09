7476a659eb2dc5ca2f80e99306e90dc1caa5536c
Pavol Zbell <pavol.zbell@gmail.com>
2014-07-09 13:06:31 +0200

    Format some code

--- sk.stuba.fiit.perconik.utilities/src/sk/stuba/fiit/perconik/utilities/MoreStrings.java
+++ sk.stuba.fiit.perconik.utilities/src/sk/stuba/fiit/perconik/utilities/MoreStrings.java
@@ -16,22 +16,103 @@ import com.google.common.base.Functions;
 public final class MoreStrings
 {
 	static final String lineSeparatorRegex = "\r?\n|\r";
-	
+
 	private MoreStrings()
 	{
 		throw new AssertionError();
 	}
 
+	public static final String lineSeparatorRegex()
+	{
+		return lineSeparatorRegex;
+	}
+
+	public static final List<String> lines(final String s)
+	{
+		return Arrays.asList(s.split(lineSeparatorRegex));
+	}
+
+	public static final String toDefaultString(Object o)
+	{
+		return o.getClass().getName() + "@" + Integer.toHexString(o.hashCode());
+	}
+
+	public static final String toCanonicalString(Object o)
+	{
+		String name = o.getClass().getCanonicalName();
+
+		if (name == null)
+		{
+			return null;
+		}
+
+		return name + "@" + Integer.toHexString(o.hashCode());
+	}
+
+	public static final String toImplementedString(Object o)
+	{
+		String result = o.toString();
+
+		if (toDefaultString(o).equals(result))
+		{
+			return null;
+		}
+
+		return result;
+	}
+
 	private enum ToStringComparator implements Comparator<Object>
 	{
 		INSTANCE;
-		
+
 		public final int compare(Object a, Object b)
 		{
 			return a.toString().compareTo(b.toString());
 		}
 	}
-	
+
+	public static final <T> Comparator<T> toStringComparator()
+	{
+		@SuppressWarnings("unchecked")
+		Comparator<T> comparator = (Comparator<T>) ToStringComparator.INSTANCE;
+
+		return comparator;
+	}
+
+	public static final String toStringFallback(Object o)
+	{
+		String result = toImplementedString(o);
+
+		if (result != null)
+		{
+			return result;
+		}
+
+		result = toCanonicalString(o);
+
+		if (result == null)
+		{
+			return result;
+		}
+
+		return toDefaultString(o);
+	}
+
+	public static final <T> Function<T, String> toStringFunction()
+	{
+		return (Function<T, String>) Functions.toStringFunction();
+	}
+
+	public static final String toLowerCaseFirst(String s)
+	{
+		return Character.toLowerCase(s.charAt(0)) + s.substring(1);
+	}
+
+	public static final String toUpperCaseFirst(String s)
+	{
+		return Character.toUpperCase(s.charAt(0)) + s.substring(1);
+	}
+
 	private enum ToLowerCaseFunction implements Function<String, String>
 	{
 		INSTANCE;
@@ -51,93 +132,12 @@ public final class MoreStrings
 			return String.valueOf(s).toUpperCase();
 		}
 	}
-	
-	public static final String lineSeparatorRegex()
-	{
-		return lineSeparatorRegex;
-	}
-	
-	public static final List<String> lines(final String s)
-	{
-		return Arrays.asList(s.split(lineSeparatorRegex));
-	}
 
-	public static final String toDefaultString(Object o)
-	{
-		return o.getClass().getName() + "@" + Integer.toHexString(o.hashCode());
-	}
-
-	public static final String toCanonicalString(Object o)
-	{
-		String name = o.getClass().getCanonicalName();
-		
-		if (name == null)
-		{
-			return null;
-		}
-		
-		return name + "@" + Integer.toHexString(o.hashCode());
-	}
-	
-	public static final String toImplementedString(Object o)
-	{
-		String result = o.toString();
-		
-		if (toDefaultString(o).equals(result))
-		{
-			return null;
-		}
-		
-		return result;
-	}
-
-	public static final <T> Comparator<T> toStringComparator()
-	{
-		@SuppressWarnings("unchecked")
-		Comparator<T> comparator = (Comparator<T>) ToStringComparator.INSTANCE;
-		
-		return comparator;
-	}
-
-	public static final String toStringFallback(Object o)
-	{
-		String result = toImplementedString(o);
-	
-		if (result != null)
-		{
-			return result;
-		}
-	
-		result = toCanonicalString(o);
-		
-		if (result == null)
-		{
-			return result;
-		}
-	
-		return toDefaultString(o);
-	}
-
-	public static final <T> Function<T, String> toStringFunction()
-	{
-		return (Function<T, String>) Functions.toStringFunction();
-	}
-	
-	public static final String toLowerCaseFirst(String s)
-	{
-		return Character.toLowerCase(s.charAt(0)) + s.substring(1);
-	}
-	
 	public static final Function<String, String> toLowerCaseFunction()
 	{
 		return ToLowerCaseFunction.INSTANCE;
 	}
 
-	public static final String toUpperCaseFirst(String s)
-	{
-		return Character.toUpperCase(s.charAt(0)) + s.substring(1);
-	}
-	
 	public static final Function<String, String> toUpperCaseFunction()
 	{
 		return ToUpperCaseFunction.INSTANCE;
