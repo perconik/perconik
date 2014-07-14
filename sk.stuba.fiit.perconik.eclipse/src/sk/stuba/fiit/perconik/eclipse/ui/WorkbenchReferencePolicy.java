a5c3972c0db71902b28e0312762ceede2dd82f3e
Pavol Zbell <pavol.zbell@gmail.com>
2014-07-14 16:11:52 +0200

    Add WorkbenchReferencePolicy

---
+++ sk.stuba.fiit.perconik.eclipse/src/sk/stuba/fiit/perconik/eclipse/ui/WorkbenchReferencePolicy.java
@@ -0,0 +1,37 @@
+package sk.stuba.fiit.perconik.eclipse.ui;
+
+import org.eclipse.ui.IEditorPart;
+import org.eclipse.ui.IEditorReference;
+import org.eclipse.ui.IViewPart;
+import org.eclipse.ui.IViewReference;
+import org.eclipse.ui.IWorkbenchPart;
+import org.eclipse.ui.IWorkbenchPartReference;
+
+public enum WorkbenchReferencePolicy
+{
+	DEFAULT(false),
+
+	RESTORE(true);
+
+	private final boolean restore;
+
+	private WorkbenchReferencePolicy(final boolean restore)
+	{
+		this.restore = restore;
+	}
+
+	public final IEditorPart getEditor(final IEditorReference reference)
+	{
+		return reference.getEditor(this.restore);
+	}
+
+	public final IWorkbenchPart getPart(final IWorkbenchPartReference reference)
+	{
+		return reference.getPart(this.restore);
+	}
+
+	public final IViewPart getView(final IViewReference reference)
+	{
+		return reference.getView(this.restore);
+	}
+}
