629dab0fb324258a3a2f6dc4a16a8dee7bbb2763
Pavol Zbell <pavol.zbell@gmail.com>
2014-07-17 13:21:10 +0200

    Add more preconditions

--- sk.stuba.fiit.perconik.utilities/src/sk/stuba/fiit/perconik/utilities/MorePreconditions.java
+++ sk.stuba.fiit.perconik.utilities/src/sk/stuba/fiit/perconik/utilities/MorePreconditions.java
@@ -1,7 +1,9 @@
 package sk.stuba.fiit.perconik.utilities;
 
 import static com.google.common.base.Preconditions.checkState;
+
 import java.util.Collection;
+
 import javax.annotation.Nullable;
 
 public final class MorePreconditions
@@ -10,46 +12,67 @@ public final class MorePreconditions
 	{
 		throw new AssertionError();
 	}
-	
+
 	public static <T extends CharSequence> T checkNotNullAndNotEmpty(T reference)
 	{
 		checkState(reference.length() > 0);
-		
+
 		return reference;
 	}
-	
+
 	public static <T extends CharSequence> T checkNotNullAndNotEmpty(T reference, @Nullable Object message)
 	{
 		checkState(reference.length() > 0, message);
-		
+
 		return reference;
 	}
-	
+
 	public static <T extends CharSequence> T checkNotNullAndNotEmpty(T reference, @Nullable String format, @Nullable Object ... args)
 	{
 		checkState(reference.length() > 0, format, args);
-		
+
 		return reference;
 	}
-	
+
 	public static <T extends Collection<?>> T checkNotNullAndNotEmpty(T reference)
 	{
 		checkState(!reference.isEmpty());
-		
+
 		return reference;
 	}
-	
+
 	public static <T extends Collection<?>> T checkNotNullAndNotEmpty(T reference, @Nullable Object message)
 	{
 		checkState(!reference.isEmpty(), message);
-		
+
 		return reference;
 	}
-	
+
 	public static <T extends Collection<?>> T checkNotNullAndNotEmpty(T reference, @Nullable String format, @Nullable Object ... args)
 	{
 		checkState(!reference.isEmpty(), format, args);
-		
+
+		return reference;
+	}
+
+	public static <T> T checkNotNullAsState(T reference)
+	{
+		checkState(reference != null);
+
+		return reference;
+	}
+
+	public static <T> T checkNotNullAsState(T reference, @Nullable Object message)
+	{
+		checkState(reference != null, message);
+
+		return reference;
+	}
+
+	public static <T> T checkNotNullAsState(T reference, @Nullable String format, @Nullable Object ... args)
+	{
+		checkState(reference != null, format, args);
+
 		return reference;
 	}
 }
