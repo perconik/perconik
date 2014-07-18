6565129e45c786cdbbbe76ffa4868cd4d17d175e
Pavol Zbell <pavol.zbell@gmail.com>
2014-07-18 10:03:24 +0200

    Fix minor stuff in JavaProjects helpers

--- sk.stuba.fiit.perconik.core.java/src/sk/stuba/fiit/perconik/core/java/JavaProjects.java
+++ sk.stuba.fiit.perconik.core.java/src/sk/stuba/fiit/perconik/core/java/JavaProjects.java
@@ -1,5 +1,7 @@
 package sk.stuba.fiit.perconik.core.java;
 
+import com.google.common.base.Preconditions;
+
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.resources.IResource;
 import org.eclipse.core.runtime.CoreException;
@@ -9,7 +11,6 @@ import org.eclipse.jdt.core.JavaCore;
 import org.eclipse.jdt.core.JavaModelException;
 import org.eclipse.jdt.ui.PreferenceConstants;
 import org.eclipse.jface.preference.IPreferenceStore;
-import com.google.common.base.Preconditions;
 
 public final class JavaProjects
 {
@@ -17,55 +18,53 @@ public final class JavaProjects
 	{
 		throw new AssertionError();
 	}
-	
+
 	public static final IJavaProject create(final IProject project)
 	{
 		Preconditions.checkState(isJavaProject(project));
-		
+
 		return JavaCore.create(project);
 	}
-	
+
 	public static final boolean inOutputLocation(final IProject project, final IResource resource)
 	{
 		if (isJavaProject(project))
 		{
 			return inOutputLocation(create(project), resource);
 		}
-		
+
 		return false;
 	}
-	
+
 	public static final boolean inOutputLocation(final IJavaProject project, final IResource resource)
 	{
-		IPath output = getOutputLocation(project);
-		
-		return resource.getFullPath().toString().startsWith(output.toString());
+		return getOutputLocation(project).isPrefixOf(resource.getFullPath());
 	}
-	
+
 	private static final IPath getDefaultLocation(final IJavaProject project, final String key)
 	{
 		IPreferenceStore store = PreferenceConstants.getPreferenceStore();
-		
+
 		if (store.getBoolean(PreferenceConstants.SRCBIN_FOLDERS_IN_NEWPROJ))
 		{
 			String name = store.getString(key);
-			
+
 			return project.getProject().getFullPath().append(name);
 		}
 
 		return project.getProject().getFullPath();
 	}
-	
+
 	public static final IPath getDefaultSourceLocation(final IJavaProject project)
 	{
 		return getDefaultLocation(project, PreferenceConstants.SRCBIN_SRCNAME);
 	}
-	
+
 	public static final IPath getDefaultOutputLocation(final IJavaProject project)
 	{
 		return getDefaultLocation(project, PreferenceConstants.SRCBIN_BINNAME);
 	}
-	
+
 	public static final IPath getOutputLocation(final IJavaProject project)
 	{
 		try
@@ -77,7 +76,7 @@ public final class JavaProjects
 			throw JavaExceptions.propagate(e);
 		}
 	}
-	
+
 	public static final boolean isJavaProject(final IProject project)
 	{
 		try
