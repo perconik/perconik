27ca43c93bc2bf1cc1a39d34098fa40b9a237999
Pavol Zbell <pavol.zbell@gmail.com>
2014-07-22 14:52:38 +0200

    Drop required EGit to 2.2

--- com.gratex.perconik.activity/src/com/gratex/perconik/activity/ide/IdeGitProjects.java
+++ com.gratex.perconik.activity/src/com/gratex/perconik/activity/ide/IdeGitProjects.java
@@ -5,10 +5,14 @@ import java.io.IOException;
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.resources.IResource;
 import org.eclipse.core.runtime.IPath;
-import org.eclipse.egit.core.RepositoryUtil;
 import org.eclipse.egit.core.project.GitProjectData;
 import org.eclipse.egit.core.project.RepositoryMapping;
+import org.eclipse.jgit.lib.FileMode;
 import org.eclipse.jgit.lib.Repository;
+import org.eclipse.jgit.treewalk.FileTreeIterator;
+import org.eclipse.jgit.treewalk.TreeWalk;
+import org.eclipse.jgit.treewalk.WorkingTreeIterator;
+import org.eclipse.jgit.treewalk.filter.PathFilter;
 
 @SuppressWarnings("restriction") // TODO resolve: not sure why is egit core restricted
 public final class IdeGitProjects
@@ -39,7 +43,31 @@ public final class IdeGitProjects
 
 	public static final boolean isIgnored(final IPath path) throws IOException
 	{
-		return RepositoryUtil.isIgnored(path);
+		// TODO find better way to resolve this dependency:
+		// RepositoryUtil.isIgnored(path) requires egit 2.3, but eclipse 3.8 has only egit 2.2
+		//return RepositoryUtil.isIgnored(path);
+		
+		// TODO resolve: code copied from egit 3.4
+		RepositoryMapping mapping = RepositoryMapping.getMapping(path);
+		if (mapping == null)
+			return true; // Linked resources may not be mapped
+		Repository repository = mapping.getRepository();
+		String repoRelativePath = mapping.getRepoRelativePath(path);
+		TreeWalk walk = new TreeWalk(repository);
+		try {
+			walk.addTree(new FileTreeIterator(repository));
+			walk.setFilter(PathFilter.create(repoRelativePath));
+			while (walk.next()) {
+				WorkingTreeIterator workingTreeIterator = walk.getTree(0, WorkingTreeIterator.class);
+				if (walk.getPathString().equals(repoRelativePath))
+					return workingTreeIterator.isEntryIgnored();
+				if (workingTreeIterator.getEntryFileMode().equals(FileMode.TREE))
+					walk.enterSubtree();
+			}
+		} finally {
+			walk.release();
+		}
+		return false;
 	}
 
 	public static final boolean isMapped(final IPath path)
