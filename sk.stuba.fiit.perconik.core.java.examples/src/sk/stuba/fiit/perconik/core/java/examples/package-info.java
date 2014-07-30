60d91c7b9704f855bbf087dce2edbcffefd4a9da
Pavol Zbell <pavol.zbell@gmail.com>
2014-07-30 12:24:45 +0200

    Remove Java DOM examples

--- sk.stuba.fiit.perconik.core.java.examples/src/sk/stuba/fiit/perconik/core/java/examples/package-info.java
+++
@@ -1,7 +0,0 @@
-/**
- * Examples of Java Model core extensions usage.
- */
-@ParametersAreNonnullByDefault
-package sk.stuba.fiit.perconik.core.java.examples;
-
-import javax.annotation.ParametersAreNonnullByDefault;
