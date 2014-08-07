fc7d4a58b84e2b04c554f511f91d758227e15e58
Pavol Zbell <pavol.zbell@gmail.com>
2014-08-07 16:16:57 +0200

    Fix bugs in UACA proxy reporter

--- com.gratex.perconik.uaca/src/com/gratex/perconik/uaca/UacaReporter.java
+++ com.gratex.perconik.uaca/src/com/gratex/perconik/uaca/UacaReporter.java
@@ -1,25 +1,38 @@
 package com.gratex.perconik.uaca;
 
 import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.LinkedHashMap;
 
 import javax.annotation.Nullable;
 import javax.ws.rs.client.WebTarget;
 
+import org.osgi.framework.Version;
+
 import com.fasterxml.jackson.core.JsonGenerationException;
 import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.json.PackageVersion;
 import com.fasterxml.jackson.core.util.DefaultPrettyPrinter;
+import com.fasterxml.jackson.databind.DeserializationContext;
 import com.fasterxml.jackson.databind.JavaType;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.ObjectWriter;
 import com.fasterxml.jackson.databind.PropertyNamingStrategy.PropertyNamingStrategyBase;
 import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.databind.deser.std.StdScalarDeserializer;
+import com.fasterxml.jackson.databind.module.SimpleModule;
+import com.fasterxml.jackson.databind.ser.std.ToStringSerializer;
 import com.fasterxml.jackson.databind.type.TypeFactory;
 import com.fasterxml.jackson.datatype.guava.GuavaModule;
 
 import com.gratex.perconik.uaca.preferences.UacaPreferences;
 import com.gratex.perconik.uaca.preferences.UacaPreferences.Keys;
 import com.gratex.perconik.uaca.ui.UacaMessageDialogs;
+
+import static java.lang.String.format;
 
 import static com.google.common.base.Strings.isNullOrEmpty;
 
@@ -28,8 +41,7 @@ final class UacaReporter {
     throw new AssertionError();
   }
 
-  private static final class ObjectHelper
-  {
+  private static final class ObjectHelper {
     private static final ObjectMapper mapper;
 
     private static final ObjectWriter writer;
@@ -39,6 +51,7 @@ final class UacaReporter {
     static {
       mapper = new ObjectMapper();
 
+      mapper.registerModule(new Module());
       mapper.registerModule(new GuavaModule());
 
       mapper.setPropertyNamingStrategy(new Naming());
@@ -55,6 +68,56 @@ final class UacaReporter {
 
     private ObjectHelper() {
       throw new AssertionError();
+    }
+
+    private static final class Module extends SimpleModule {
+      private static final long serialVersionUID = 0L;
+
+      public Module() {
+        super(PackageVersion.VERSION);
+
+        this.addSerializer(Path.class, ToStringSerializer.instance);
+        this.addSerializer(Version.class, ToStringSerializer.instance);
+
+        this.addDeserializer(Path.class, new PathDeserializer());
+        this.addDeserializer(Version.class, new VersionDeserializer());
+      }
+
+      @Override
+      public final boolean equals(Object o) {
+        return this == o;
+      }
+
+      @Override
+      public final int hashCode() {
+        return Module.class.hashCode();
+      }
+
+      private static final class PathDeserializer extends StdScalarDeserializer<Path> {
+        private static final long serialVersionUID = 0L;
+
+        public PathDeserializer() {
+          super(Path.class);
+        }
+
+        @Override
+        public final Path deserialize(final JsonParser parser, final DeserializationContext context) throws IOException, JsonProcessingException {
+          return Paths.get(parser.getValueAsString());
+        }
+      }
+
+      private static final class VersionDeserializer extends StdScalarDeserializer<Version> {
+        private static final long serialVersionUID = 0L;
+
+        public VersionDeserializer() {
+          super(Version.class);
+        }
+
+        @Override
+        public final Version deserialize(final JsonParser parser, final DeserializationContext context) throws IOException, JsonProcessingException {
+          return Version.parseVersion(parser.getValueAsString());
+        }
+      }
     }
 
     private static final class Naming extends PropertyNamingStrategyBase {
@@ -107,9 +170,9 @@ final class UacaReporter {
     }
 
     try {
-      UacaConsole.getInstance().notice(String.format("%s%n%s", target.getUri(), ObjectHelper.toString(request)));
+      UacaConsole.getInstance().notice(format("%s%n%s", target.getUri(), ObjectHelper.toString(request)));
     } catch (Exception e) {
-      UacaConsole.getInstance().error(e, "Event logger failed");
+      UacaConsole.getInstance().error(e, "UacaProxy: Unable to format object");
     }
   }
 
@@ -122,8 +185,10 @@ final class UacaReporter {
   }
 
   static final void displayError(final String message, @Nullable final Exception failure) {
-    if (UacaPreferences.getShared().getPreferenceStore().getBoolean(Keys.displayErrors)) {
-      UacaMessageDialogs.openError(Keys.displayErrors, isNullOrEmpty(message) ? failure.getMessage() : message);
+    if (!UacaPreferences.getShared().getPreferenceStore().getBoolean(Keys.displayErrors)) {
+      return;
     }
+
+    UacaMessageDialogs.openError(Keys.displayErrors, isNullOrEmpty(message) ? failure.getMessage() : message);
   }
 }
