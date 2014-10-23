ba96f84a10dfdca7304536e545c0e5b643f47009
Pavol Zbell <pavol.zbell@gmail.com>
2014-10-23 04:07:49 +0200

    Improve PlatformExecutors helper

--- sk.stuba.fiit.perconik.utilities/src/sk/stuba/fiit/perconik/utilities/concurrent/PlatformExecutors.java
+++ sk.stuba.fiit.perconik.utilities/src/sk/stuba/fiit/perconik/utilities/concurrent/PlatformExecutors.java
@@ -1,38 +1,47 @@
 package sk.stuba.fiit.perconik.utilities.concurrent;
 
 import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
 import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.ThreadFactory;
 import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
 
+import static java.lang.Math.max;
 import static java.lang.Math.min;
+import static java.lang.Math.round;
+import static java.lang.Runtime.getRuntime;
+import static java.util.concurrent.Executors.defaultThreadFactory;
+
+import static com.google.common.base.Preconditions.checkArgument;
 
 public final class PlatformExecutors {
   private static final int availableProcessorsUpperBound = 32;
 
   private static final float maximumPoolSizeScalingFactor = 10;
 
+  private static final int minimumPoolSize = 2;
+
   private PlatformExecutors() {}
 
   private static int boundedAvailableProcessors() {
-    return min(Runtime.getRuntime().availableProcessors(), availableProcessorsUpperBound);
+    return min(getRuntime().availableProcessors(), availableProcessorsUpperBound);
   }
 
-  private static int maximumPoolSize() {
-    return maximumPoolSize(maximumPoolSizeScalingFactor);
-  }
-
-  private static int maximumPoolSize(final float factor) {
-    return Math.round(factor * boundedAvailableProcessors());
+  private static int maximumPoolSize(final float poolSizeScalingFactor) {
+    return max(round(min(poolSizeScalingFactor, maximumPoolSizeScalingFactor) * boundedAvailableProcessors()), minimumPoolSize);
   }
 
   public static ExecutorService newLimitedThreadPool() {
-    return newLimitedThreadPool(Executors.defaultThreadFactory());
+    return newLimitedThreadPool(maximumPoolSizeScalingFactor);
   }
 
-  public static ExecutorService newLimitedThreadPool(final ThreadFactory factory) {
-    return new ThreadPoolExecutor(0, maximumPoolSize(), 60L, TimeUnit.SECONDS, new LinkedBlockingQueue<Runnable>(), factory);
+  public static ExecutorService newLimitedThreadPool(final float poolSizeScalingFactor) {
+    return newLimitedThreadPool(poolSizeScalingFactor, defaultThreadFactory());
+  }
+
+  public static ExecutorService newLimitedThreadPool(final float poolSizeScalingFactor, final ThreadFactory factory) {
+    checkArgument(poolSizeScalingFactor >= 0);
+
+    return new ThreadPoolExecutor(0, maximumPoolSize(poolSizeScalingFactor), 60L, TimeUnit.SECONDS, new LinkedBlockingQueue<Runnable>(), factory);
   }
 }
