2b5bd6cb35d3f75bd1d25cb73f13769d9cf202b7
Pavol Zbell <pavol.zbell@gmail.com>
2014-10-25 22:58:18 +0200

    Fix structured content key helpers

--- sk.stuba.fiit.perconik.data/src/sk/stuba/fiit/perconik/data/content/StructuredContents.java
+++ sk.stuba.fiit.perconik.data/src/sk/stuba/fiit/perconik/data/content/StructuredContents.java
@@ -3,12 +3,21 @@ package sk.stuba.fiit.perconik.data.content;
 import java.util.Iterator;
 
 import com.google.common.base.Joiner;
+import com.google.common.base.Predicate;
 import com.google.common.base.Splitter;
 
+import static com.google.common.base.Predicates.not;
+import static com.google.common.collect.Iterables.filter;
+import static com.google.common.collect.Iterators.filter;
+import static com.google.common.collect.Lists.asList;
+
 import static sk.stuba.fiit.perconik.data.content.StructuredContent.separator;
+import static sk.stuba.fiit.perconik.utilities.MoreStrings.isNullOrEmptyPredicate;
 
 public class StructuredContents {
-  private static final Joiner joiner = Joiner.on(separator).skipNulls();
+  private static final Joiner joiner = Joiner.on(separator);
+
+  private static final Predicate<String> filter = not(isNullOrEmptyPredicate());
 
   private static final Splitter splitter = Splitter.on(separator).trimResults().omitEmptyStrings();
 
@@ -16,15 +25,15 @@ public class StructuredContents {
   }
 
   public static String key(final String first, final String second, final String ... rest) {
-    return joiner.join(first, second, (Object[]) rest);
+    return key(asList(first, second, rest));
   }
 
   public static String key(final Iterable<String> sequence) {
-    return joiner.join(sequence);
+    return joiner.join(filter(sequence, filter));
   }
 
   public static String key(final Iterator<String> sequence) {
-    return joiner.join(sequence);
+    return joiner.join(filter(sequence, filter));
   }
 
   public static Iterable<String> sequence(final CharSequence key) {
