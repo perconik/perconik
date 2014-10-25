11ecfc81a4e7cb73399372f92b02d646a1bdd34b
Pavol Zbell <pavol.zbell@gmail.com>
2014-10-25 13:10:40 +0200

    Fix UACA proxy

--- sk.stuba.fiit.perconik.activity/src/sk/stuba/fiit/perconik/activity/uaca/UacaProxy.java
+++ sk.stuba.fiit.perconik.activity/src/sk/stuba/fiit/perconik/activity/uaca/UacaProxy.java
@@ -7,14 +7,14 @@ import com.gratex.perconik.uaca.SharedUacaProxy;
 import sk.stuba.fiit.perconik.data.content.Content;
 import sk.stuba.fiit.perconik.data.store.Store;
 
-public final class UacaProxy extends SharedUacaProxy implements Store {
+public final class UacaProxy extends SharedUacaProxy implements Store<Object> {
   public UacaProxy() {}
 
-  public Content load(final String path, @Nullable final Content request) {
+  public Content load(final String path, @Nullable final Object request) {
     throw new UnsupportedOperationException();
   }
 
-  public void save(final String path, @Nullable final Content resource) {
+  public void save(final String path, @Nullable final Object resource) {
     this.send("generic/event", UacaDataWrapper.of(path, resource));
   }
 }
