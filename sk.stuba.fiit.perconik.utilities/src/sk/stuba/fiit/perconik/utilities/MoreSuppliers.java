f7599e30784d48196bd7c192f977d48d88da1059
Pavol Zbell <pavol.zbell@gmail.com>
2014-11-04 19:35:04 +0100

    Add more supplier helpers

--- sk.stuba.fiit.perconik.utilities/src/sk/stuba/fiit/perconik/utilities/MoreSuppliers.java
+++ sk.stuba.fiit.perconik.utilities/src/sk/stuba/fiit/perconik/utilities/MoreSuppliers.java
@@ -4,8 +4,19 @@ import java.util.concurrent.Callable;
 
 import com.google.common.base.Supplier;
 
+import static com.google.common.base.Suppliers.ofInstance;
+
 public final class MoreSuppliers {
+  private static final Supplier<Object> ofNull = ofInstance(null);
+
   private MoreSuppliers() {}
+
+  public static <T> Supplier<T> ofNull() {
+    @SuppressWarnings("unchecked")
+    Supplier<T> casted = (Supplier<T>) ofNull;
+
+    return casted;
+  }
 
   public static <V> Callable<V> asCallable(final Supplier<V> supplier) {
     return new Callable<V>() {
