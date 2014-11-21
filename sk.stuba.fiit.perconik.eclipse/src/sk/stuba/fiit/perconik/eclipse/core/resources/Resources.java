08e0b82bdd1aa76ddaa0bcc99901be68672de536
Pavol Zbell <pavol.zbell@gmail.com>
2014-11-21 04:39:01 +0100

    Add more resource helpers

--- sk.stuba.fiit.perconik.eclipse/src/sk/stuba/fiit/perconik/eclipse/core/resources/Resources.java
+++ sk.stuba.fiit.perconik.eclipse/src/sk/stuba/fiit/perconik/eclipse/core/resources/Resources.java
@@ -1,11 +1,15 @@
 package sk.stuba.fiit.perconik.eclipse.core.resources;
 
+import java.net.URI;
 import java.util.EnumSet;
 import java.util.Set;
+
+import javax.annotation.Nullable;
 
 import org.eclipse.core.resources.IResource;
 import org.eclipse.core.resources.IResourceVisitor;
 import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IPath;
 
 import sk.stuba.fiit.perconik.eclipse.core.runtime.CoreExceptions;
 
@@ -36,5 +40,13 @@ public final class Resources {
     } catch (CoreException e) {
       CoreExceptions.propagate(e);
     }
+  }
+
+  public static IPath getLocation(@Nullable final IResource resource) {
+    return resource != null ? resource.getLocation() : null;
+  }
+
+  public static URI getLocationUri(@Nullable final IResource resource) {
+    return resource != null ? resource.getLocationURI() : null;
   }
 }
