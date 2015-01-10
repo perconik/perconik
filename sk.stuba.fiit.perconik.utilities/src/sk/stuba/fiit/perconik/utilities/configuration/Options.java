d1911bcb45103df9c3e669641b7a9bd5eee01b26
Pavol Zbell <pavol.zbell@gmail.com>
2015-01-10 22:04:00 +0100

    Add documentation to Options

--- sk.stuba.fiit.perconik.utilities/src/sk/stuba/fiit/perconik/utilities/configuration/Options.java
+++ sk.stuba.fiit.perconik.utilities/src/sk/stuba/fiit/perconik/utilities/configuration/Options.java
@@ -9,7 +9,24 @@ import java.util.Map;
  * @since 1.0
  */
 public interface Options {
+  /**
+   * Loads options from map to this options instance.
+   *
+   * <p>Note that the implementation specifies whether
+   * this method extracts map entries or provides a view of the map.
+   *
+   * @throws UnsupportedOperationException if the options
+   *         does not support loading new values
+   */
   public void fromMap(Map<String, Object> map);
 
+  /**
+   * Converts this options instance to map.
+   *
+   * <p>Note that the implementation specifies whether
+   * this method returns a snapshot or view of options.
+   *
+   * @return either a snapshot or view of options
+   */
   public Map<String, Object> toMap();
 }
