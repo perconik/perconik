2c35e2846345c1306816050cdb65ed3175a518a7
Pavol Zbell <pavol.zbell@gmail.com>
2015-01-12 09:38:43 +0100

    Fix package info

--- sk.stuba.fiit.perconik.utilities/src/sk/stuba/fiit/perconik/utilities/configuration/package-info.java
+++ sk.stuba.fiit.perconik.utilities/src/sk/stuba/fiit/perconik/utilities/configuration/package-info.java
@@ -1,7 +1,8 @@
 /**
- * Utilities extending Java concurrent capabilities.
+ * Utilities extending Java configuration capabilities.
  */
 @ParametersAreNonnullByDefault
 package sk.stuba.fiit.perconik.utilities.configuration;
 
 import javax.annotation.ParametersAreNonnullByDefault;
+
