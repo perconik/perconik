3b9f147e748dc12a7d59997fde2bae24d6641c69
Pavol Zbell <pavol.zbell@gmail.com>
2015-01-12 19:26:39 +0100

    Improve SortDirection helper

--- sk.stuba.fiit.perconik.eclipse/src/sk/stuba/fiit/perconik/eclipse/swt/SortDirection.java
+++ sk.stuba.fiit.perconik.eclipse/src/sk/stuba/fiit/perconik/eclipse/swt/SortDirection.java
@@ -1,11 +1,37 @@
 package sk.stuba.fiit.perconik.eclipse.swt;
 
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
 import org.eclipse.swt.SWT;
 
-public enum SortDirection {
-  UP(SWT.UP),
+import static java.util.Collections.reverseOrder;
 
-  DOWN(SWT.DOWN);
+public enum SortDirection {
+  UP(SWT.UP) {
+    @Override
+    public <T extends Comparable<? super T>> void sort(final List<T> list) {
+      Collections.sort(list);
+    }
+
+    @Override
+    public <T> void sort(final List<T> list, final Comparator<? super T> comparator) {
+      Collections.sort(list, comparator);
+    }
+  },
+
+  DOWN(SWT.DOWN) {
+    @Override
+    public <T extends Comparable<? super T>> void sort(final List<T> list) {
+      Collections.sort(list, reverseOrder());
+    }
+
+    @Override
+    public <T> void sort(final List<T> list, final Comparator<? super T> comparator) {
+      Collections.sort(list, reverseOrder(comparator));
+    }
+  };
 
   private final int value;
 
@@ -16,6 +42,10 @@ public enum SortDirection {
   public SortDirection opposite() {
     return this == UP ? DOWN : UP;
   }
+
+  public abstract <T extends Comparable<? super T>> void sort(List<T> list);
+
+  public abstract <T> void sort(List<T> list, Comparator<? super T> comparator);
 
   public int getValue() {
     return this.value;
