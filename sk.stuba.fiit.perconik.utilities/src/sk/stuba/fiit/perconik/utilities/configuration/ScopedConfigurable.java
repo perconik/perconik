443af86e860a0d2ab5031a027142fcddb12326bb
Pavol Zbell <pavol.zbell@gmail.com>
2015-02-15 18:56:49 +0100

    Update ScopedConfigurable specification

--- sk.stuba.fiit.perconik.utilities/src/sk/stuba/fiit/perconik/utilities/configuration/ScopedConfigurable.java
+++ sk.stuba.fiit.perconik.utilities/src/sk/stuba/fiit/perconik/utilities/configuration/ScopedConfigurable.java
@@ -16,6 +16,9 @@ public interface ScopedConfigurable extends Configurable {
    * @param scope the scope to be applied, not {@code null}
    *
    * @return either a snapshot or view of options
+   *
+   * @throws IllegalArgumentException if an unknown scope is specified
+   * @throws NullPointerException if the {@code scope} is {@code null}
    */
   public Options getOptions(Scope scope);
 }
