b94a382099915c8ab0cc8b2d9c8ebf003cfb0351
Pavol Zbell <pavol.zbell@gmail.com>
2015-02-23 13:41:26 +0100

    Fix text input change for document resource

--- sk.stuba.fiit.perconik.core/src/sk/stuba/fiit/perconik/core/resources/DocumentHook.java
+++ sk.stuba.fiit.perconik.core/src/sk/stuba/fiit/perconik/core/resources/DocumentHook.java
@@ -1,17 +1,29 @@
 package sk.stuba.fiit.perconik.core.resources;
 
+import java.util.Map;
+import java.util.Map.Entry;
+
 import org.eclipse.jface.text.IDocument;
+import org.eclipse.jface.text.ITextViewer;
+import org.eclipse.ui.IEditorPart;
 import org.eclipse.ui.IEditorReference;
 
+import sk.stuba.fiit.perconik.core.adapters.TextInputAdapter;
 import sk.stuba.fiit.perconik.core.listeners.DocumentListener;
 import sk.stuba.fiit.perconik.core.listeners.EditorListener;
 import sk.stuba.fiit.perconik.eclipse.ui.Editors;
 
+import static com.google.common.collect.Maps.newHashMap;
+
 import static sk.stuba.fiit.perconik.core.resources.Ui.dereferenceEditor;
 
 final class DocumentHook extends InternalHook<IDocument, DocumentListener> implements EditorListener {
+  private final Map<ITextViewer, TextInputChangeFix> fixes;
+
   DocumentHook(final DocumentListener listener) {
     super(new DocumentHandler(listener));
+
+    this.fixes = newHashMap();
   }
 
   static final class Support extends AbstractHookSupport<DocumentHook, IDocument, DocumentListener> {
@@ -34,17 +46,66 @@ final class DocumentHook extends InternalHook<IDocument, DocumentListener> imple
     }
   }
 
+  private final class TextInputChangeFix extends TextInputAdapter {
+    TextInputChangeFix() {}
+
+    @Override
+    public void inputDocumentChanged(final IDocument before, final IDocument after) {
+      Hooks.removeNonNull(DocumentHook.this, before);
+      Hooks.addNonNull(DocumentHook.this, after);
+    }
+  }
+
   @Override
   void preRegisterInternal() {
     Hooks.addDocumentsAsynchronouslyTo(this);
   }
 
+  @Override
+  void postUnregisterInternal() {
+    synchronized (this.fixes) {
+      for (Entry<ITextViewer, TextInputChangeFix> entry: this.fixes.entrySet()) {
+        entry.getKey().removeTextInputListener(entry.getValue());
+      }
+    }
+  }
+
   public void editorOpened(final IEditorReference reference) {
-    Hooks.addNonNull(this, Editors.getDocument(dereferenceEditor(reference)));
+    IEditorPart editor = dereferenceEditor(reference);
+    ITextViewer viewer = Editors.getTextViewer(editor);
+    IDocument document = Editors.getDocument(viewer);
+
+    if (viewer != null && document != null) {
+      synchronized (this.fixes) {
+        if (!this.fixes.containsKey(viewer)) {
+          TextInputChangeFix fix = new TextInputChangeFix();
+
+          this.fixes.put(viewer, fix);
+
+          viewer.addTextInputListener(fix);
+        }
+      }
+
+      this.add(document);
+    }
   }
 
   public void editorClosed(final IEditorReference reference) {
-    Hooks.removeNonNull(this, Editors.getDocument(dereferenceEditor(reference)));
+    IEditorPart editor = dereferenceEditor(reference);
+    ITextViewer viewer = Editors.getTextViewer(editor);
+    IDocument document = Editors.getDocument(viewer);
+
+    if (viewer != null) {
+      synchronized (this.fixes) {
+        TextInputChangeFix fix = this.fixes.remove(viewer);
+
+        viewer.removeTextInputListener(fix);
+      }
+    }
+
+    if (document != null) {
+      this.remove(document);
+    }
   }
 
   public void editorActivated(final IEditorReference reference) {}
