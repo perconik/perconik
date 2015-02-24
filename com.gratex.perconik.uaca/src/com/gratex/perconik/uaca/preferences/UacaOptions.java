0b0ccd74e324f6d8b08dab4e5dfbc105432ea206
Pavol Zbell <pavol.zbell@gmail.com>
2015-02-24 03:19:51 +0100

    Refactor UacaOptions

--- com.gratex.perconik.uaca/src/com/gratex/perconik/uaca/preferences/UacaOptions.java
+++ com.gratex.perconik.uaca/src/com/gratex/perconik/uaca/preferences/UacaOptions.java
@@ -3,18 +3,24 @@ package com.gratex.perconik.uaca.preferences;
 import java.net.URL;
 import java.util.Map;
 
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.Maps;
+
 import sk.stuba.fiit.perconik.utilities.configuration.AbstractOptions;
 import sk.stuba.fiit.perconik.utilities.configuration.OptionAccessor;
 import sk.stuba.fiit.perconik.utilities.configuration.Options;
 
 import static java.util.Objects.requireNonNull;
 
-import static com.google.common.collect.Maps.newLinkedHashMap;
+import static com.google.common.collect.ImmutableList.copyOf;
 
 import static com.gratex.perconik.uaca.plugin.Activator.PLUGIN_ID;
 
 import static sk.stuba.fiit.perconik.preferences.AbstractPreferences.Keys.join;
+import static sk.stuba.fiit.perconik.utilities.configuration.Configurables.mappings;
 import static sk.stuba.fiit.perconik.utilities.configuration.Configurables.option;
+import static sk.stuba.fiit.perconik.utilities.configuration.Configurables.rawValues;
+import static sk.stuba.fiit.perconik.utilities.configuration.Configurables.wildcardAccessorType;
 import static sk.stuba.fiit.perconik.utilities.configuration.OptionParsers.booleanParser;
 import static sk.stuba.fiit.perconik.utilities.configuration.OptionParsers.urlParser;
 import static sk.stuba.fiit.perconik.utilities.net.UniformResources.newUrl;
@@ -22,6 +28,8 @@ import static sk.stuba.fiit.perconik.utilities.net.UniformResources.newUrl;
 public interface UacaOptions extends Options {
   public static final class Schema {
     static final String qualifier = join(PLUGIN_ID, "preferences");
+
+    public static final ImmutableList<OptionAccessor<?>> accessors = copyOf(mappings(Schema.class, wildcardAccessorType()));
 
     public static final OptionAccessor<URL> applicationUrl = option(urlParser(), join(qualifier, "applicationUrl"), newUrl("http://localhost:16375"));
 
@@ -36,23 +44,19 @@ public interface UacaOptions extends Options {
     private Schema() {}
 
     static Map<String, Object> toMap(final UacaOptions options) {
-      Map<String, Object> map = newLinkedHashMap();
-
-      map.put(applicationUrl.getKey(), options.getApplicationUrl());
-      map.put(checkConnection.getKey(), options.isConnectionCheckEnabled());
-      map.put(displayErrors.getKey(), options.isErrorDialogEnabled());
-      map.put(logErrors.getKey(), options.isErrorLogEnabled());
-      map.put(logEvents.getKey(), options.isEventLogEnabled());
-
-      return map;
+      return rawValues(accessors, options, Maps.<String, Object>newLinkedHashMap());
     }
   }
 
   public static final class View extends AbstractOptions implements UacaOptions {
     private final Options options;
 
-    public View(final Options options) {
+    private View(final Options options) {
       this.options = requireNonNull(options);
+    }
+
+    public static UacaOptions of(final Options options) {
+      return new View(options);
     }
 
     public Map<String, Object> toMap() {
