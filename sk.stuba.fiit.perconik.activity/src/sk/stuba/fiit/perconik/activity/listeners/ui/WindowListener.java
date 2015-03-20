7a439d788770a8bbcaa4b884a67a904b68fc45a1
Pavol Zbell <pavol.zbell@gmail.com>
2015-03-20 11:39:02 +0100

    Generate open event for active window on post startup

--- sk.stuba.fiit.perconik.activity/src/sk/stuba/fiit/perconik/activity/listeners/ui/WindowListener.java
+++ sk.stuba.fiit.perconik.activity/src/sk/stuba/fiit/perconik/activity/listeners/ui/WindowListener.java
@@ -1,5 +1,7 @@
 package sk.stuba.fiit.perconik.activity.listeners.ui;
 
+import com.google.common.base.Stopwatch;
+
 import org.eclipse.ui.IWorkbench;
 import org.eclipse.ui.IWorkbenchWindow;
 
@@ -7,9 +9,12 @@ import sk.stuba.fiit.perconik.activity.events.Event;
 import sk.stuba.fiit.perconik.activity.events.LocalEvent;
 import sk.stuba.fiit.perconik.activity.listeners.CommonEventListener;
 import sk.stuba.fiit.perconik.activity.serializers.ui.WindowSerializer;
-import sk.stuba.fiit.perconik.core.annotations.Unsupported;
+import sk.stuba.fiit.perconik.core.Listeners;
 import sk.stuba.fiit.perconik.core.annotations.Version;
 import sk.stuba.fiit.perconik.eclipse.swt.widgets.DisplayTask;
+import sk.stuba.fiit.perconik.utilities.concurrent.TimeValue;
+
+import static java.util.concurrent.TimeUnit.SECONDS;
 
 import static sk.stuba.fiit.perconik.activity.listeners.ui.WindowListener.Action.ACTIVATE;
 import static sk.stuba.fiit.perconik.activity.listeners.ui.WindowListener.Action.CLOSE;
@@ -18,6 +23,8 @@ import static sk.stuba.fiit.perconik.activity.listeners.ui.WindowListener.Action
 import static sk.stuba.fiit.perconik.activity.serializers.ConfigurableSerializer.StandardOption.TREE;
 import static sk.stuba.fiit.perconik.activity.serializers.Serializations.identifyObject;
 import static sk.stuba.fiit.perconik.data.content.StructuredContents.key;
+import static sk.stuba.fiit.perconik.eclipse.ui.Workbenches.waitForActiveWindow;
+import static sk.stuba.fiit.perconik.utilities.concurrent.TimeValue.of;
 
 /**
  * TODO
@@ -25,9 +32,10 @@ import static sk.stuba.fiit.perconik.data.content.StructuredContents.key;
  * @author Pavol Zbell
  * @since 1.0
  */
-@Version("0.0.0.alpha")
-@Unsupported
-public final class WindowListener extends CommonEventListener implements sk.stuba.fiit.perconik.core.listeners.WindowListener {
+@Version("0.0.1.alpha")
+public final class WindowListener extends CommonEventListener implements sk.stuba.fiit.perconik.core.listeners.WindowListener, sk.stuba.fiit.perconik.core.listeners.WorkbenchListener {
+  static final TimeValue windowOpenOnStartupTimeout = of(10, SECONDS);
+
   public WindowListener() {}
 
   enum Action implements CommonEventListener.Action {
@@ -73,6 +81,14 @@ public final class WindowListener extends CommonEventListener implements sk.stub
     this.send(action.getPath(), build(time, action, window));
   }
 
+  void execute(final long time, final Action action, final IWorkbench workbench) {
+    this.execute(DisplayTask.of(new Runnable() {
+      public void run() {
+        process(time, action, waitForActiveWindow(workbench));
+      }
+    }));
+  }
+
   void execute(final long time, final Action action, final IWorkbenchWindow window) {
     this.execute(DisplayTask.of(new Runnable() {
       public void run() {
@@ -90,7 +106,9 @@ public final class WindowListener extends CommonEventListener implements sk.stub
   public void windowClosed(final IWorkbenchWindow window) {
     final long time = this.currentTime();
 
-    this.execute(time, CLOSE, window);
+    if (!WorkbenchState.SHUTDOWN.isProcessed()) {
+      this.execute(time, CLOSE, window);
+    }
   }
 
   public void windowActivated(final IWorkbenchWindow window) {
@@ -103,5 +121,38 @@ public final class WindowListener extends CommonEventListener implements sk.stub
     final long time = this.currentTime();
 
     this.execute(time, DEACTIVATE, window);
+  }
+
+  public void postStartup(final IWorkbench workbench) {
+    this.execute(new Runnable() {
+      public void run() {
+        if (!Listeners.registered(WorkbenchListener.class).isEmpty()) {
+          return;
+        }
+
+        TimeHelper helper = getTimeHelper();
+        TimeValue timeout = windowOpenOnStartupTimeout;
+
+        Stopwatch stopwatch = helper.createStopwatch();
+
+        while (!WorkbenchState.STARTUP.isProcessed() || timeout.duration() <= stopwatch.elapsed(timeout.unit())) {}
+
+        if (WorkbenchState.STARTUP.isProcessed()) {
+          final long time = helper.currentTime();
+
+          execute(time, OPEN, workbench);
+        }
+      }
+    });
+  }
+
+  public boolean preShutdown(final IWorkbench workbench, final boolean forced) {
+    // ignore
+
+    return true;
+  }
+
+  public void postShutdown(final IWorkbench workbench) {
+    // ignore
   }
 }
