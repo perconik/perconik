b5b159afa94624f9f526e2511265acd0c35fbfdd
Pavol Zbell <pavol.zbell@gmail.com>
2015-03-28 18:36:53 +0100

    Support consoles in text selection listener

--- sk.stuba.fiit.perconik.activity/src/sk/stuba/fiit/perconik/activity/listeners/ui/text/TextSelectionListener.java
+++ sk.stuba.fiit.perconik.activity/src/sk/stuba/fiit/perconik/activity/listeners/ui/text/TextSelectionListener.java
@@ -2,24 +2,28 @@ package sk.stuba.fiit.perconik.activity.listeners.ui.text;
 
 import java.util.LinkedList;
 
+import org.eclipse.jface.text.IDocument;
 import org.eclipse.jface.text.ITextSelection;
-import org.eclipse.ui.IEditorPart;
+import org.eclipse.jface.text.ITextViewer;
+import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.ui.IWorkbenchPart;
+import org.eclipse.ui.IWorkbenchPartReference;
 
 import sk.stuba.fiit.perconik.activity.events.Event;
 import sk.stuba.fiit.perconik.activity.listeners.ActivityListener;
 import sk.stuba.fiit.perconik.activity.serializers.ui.selection.TextSelectionSerializer;
+import sk.stuba.fiit.perconik.activity.serializers.ui.text.LineRegionSerializer;
 import sk.stuba.fiit.perconik.core.annotations.Version;
-import sk.stuba.fiit.perconik.eclipse.jdt.ui.UnderlyingView;
+import sk.stuba.fiit.perconik.core.listeners.PartListener;
 import sk.stuba.fiit.perconik.eclipse.jface.text.LineRegion;
-import sk.stuba.fiit.perconik.utilities.concurrent.NamedRunnable;
+import sk.stuba.fiit.perconik.eclipse.ui.Parts;
 import sk.stuba.fiit.perconik.utilities.concurrent.TimeValue;
 
 import static java.util.concurrent.TimeUnit.MILLISECONDS;
 import static java.util.concurrent.TimeUnit.SECONDS;
 
-import static sk.stuba.fiit.perconik.activity.listeners.AbstractListener.RegistrationHook.PRE_UNREGISTER;
 import static sk.stuba.fiit.perconik.activity.listeners.ui.text.TextSelectionListener.Action.SELECT;
+import static sk.stuba.fiit.perconik.activity.serializers.ui.Ui.dereferencePart;
 import static sk.stuba.fiit.perconik.data.content.StructuredContents.key;
 import static sk.stuba.fiit.perconik.utilities.concurrent.TimeValue.of;
 
@@ -29,22 +33,20 @@ import static sk.stuba.fiit.perconik.utilities.concurrent.TimeValue.of;
  * @author Pavol Zbell
  * @since 1.0
  */
-@Version("0.0.2.alpha")
-public final class TextSelectionListener extends AbstractTextListener implements sk.stuba.fiit.perconik.core.listeners.TextSelectionListener {
-  static final TimeValue selectionEventPause = of(500, MILLISECONDS);
+@Version("0.0.4.alpha")
+public final class TextSelectionListener extends AbstractTextListener implements PartListener, sk.stuba.fiit.perconik.core.listeners.TextSelectionListener {
+  // TODO fails on shutdown if both this and text view listener are processing pending events
 
-  static final TimeValue selectionEventWindow = of(10, SECONDS);
+  // TODO note that select must be initiated by user after startup to be sent on shutdown
+
+  static final TimeValue selectionEventPause = of(500L, MILLISECONDS);
+
+  static final TimeValue selectionEventWindow = of(10L, SECONDS);
 
   private final TextSelectionEvents events;
 
   public TextSelectionListener() {
     this.events = new TextSelectionEvents(this);
-
-    PRE_UNREGISTER.add(this, new NamedRunnable(this.getClass(), "UnsentSelectionHandler") {
-      public void run() {
-        handleUnsentSelectionOnUnregistration();
-      }
-    });
   }
 
   enum Action implements ActivityListener.Action {
@@ -68,30 +70,30 @@ public final class TextSelectionListener extends AbstractTextListener implements
     }
   }
 
-  Event build(final long time, final Action action, final IEditorPart editor, final UnderlyingView<?> view, final LineRegion region, final ITextSelection selection) {
-    Event data = this.build(time, action, editor, view, region);
+  Event build(final long time, final Action action, final LinkedList<TextSelectionEvent> sequence, final IWorkbenchPart part, final ITextSelection selection, final LineRegion region) {
+    assert sequence.getLast().selection.equals(selection);
 
-    data.put(key("selection"), new TextSelectionSerializer().serialize(selection));
+    Event data = this.build(time, action, part);
+
+    data.put(key("selection", "events", "first", "timestamp"), sequence.getFirst().time);
+    data.put(key("selection", "events", "first", "raw"), new TextSelectionSerializer().serialize(sequence.getFirst().selection));
+
+    data.put(key("selection", "events", "last", "timestamp"), sequence.getLast().time);
+    data.put(key("selection", "events", "last", "raw"), new TextSelectionSerializer().serialize(sequence.getLast().selection));
+
+    data.put(key("selection", "events", "count"), sequence.size());
+
+    data.put(key("selection", "region"), new LineRegionSerializer().serialize(region));
 
     return data;
   }
 
-  void process(final long time, final Action action, final IWorkbenchPart part, final ITextSelection selection) {
-    if (!(part instanceof IEditorPart)) {
-      return;
-    }
+  void process(final long time, final Action action, final LinkedList<TextSelectionEvent> sequence, final IWorkbenchPart part, final ITextSelection selection) {
+    IDocument document = Parts.getDocument(TextViewerSupport.getTextViewer(part));
 
-    IEditorPart editor = (IEditorPart) part;
+    LineRegion region = LineRegion.compute(document, selection.getOffset(), selection.getLength(), selection.getText()).normalize();
 
-    UnderlyingView<?> view = UnderlyingView.from(editor);
-
-    if (view == null) {
-      return;
-    }
-
-    LineRegion region = LineRegion.compute(view.getDocument(), selection.getOffset(), selection.getLength(), selection.getText());
-
-    this.send(action.getPath(), this.build(time, action, editor, view, region, selection));
+    this.send(action.getPath(), this.build(time, action, sequence, part, selection, region));
   }
 
   static final class TextSelectionEvents extends ContinuousEvent<TextSelectionListener, TextSelectionEvent> {
@@ -101,6 +103,16 @@ public final class TextSelectionListener extends AbstractTextListener implements
 
     @Override
     protected boolean accept(final LinkedList<TextSelectionEvent> sequence, final TextSelectionEvent event) {
+      IDocument document = Parts.getDocument(TextViewerSupport.getTextViewer(event.part));
+
+      if (document == null) {
+        if (this.log.isEnabled()) {
+          this.log.print("%s: document not found for %s -> ignore", "selection", event.part);
+        }
+
+        return false;
+      }
+
       boolean empty = event.isSelectionTextEmpty();
 
       if (sequence.isEmpty()) {
@@ -127,29 +139,105 @@ public final class TextSelectionListener extends AbstractTextListener implements
 
     @Override
     protected void process(final LinkedList<TextSelectionEvent> sequence) {
-      this.listener.handleAcceptedSelection(sequence.getLast());
+      this.listener.handleSelectionEvents(sequence);
     }
   }
 
-  void handleAcceptedSelection(final TextSelectionEvent event) {
-    execute(new Runnable() {
+  void handleSelectionEvents(final LinkedList<TextSelectionEvent> sequence) {
+    this.execute(new Runnable() {
       public void run() {
-        process(event.time, SELECT, event.part, event.selection);
+        TextSelectionEvent event = sequence.getLast();
+
+        process(event.time, SELECT, sequence, event.part, event.selection);
       }
     });
   }
 
-  void handleUnsentSelectionOnUnregistration() {
+  public void partOpened(final IWorkbenchPartReference reference) {
+    // ignore
+  }
+
+  public void partClosed(final IWorkbenchPartReference reference) {
+    // ignore
+  }
+
+  public void partActivated(final IWorkbenchPartReference reference) {
+    // ensures that a selection change is always generated on part activation,
+    // this primarily helps handling selection context switch in some cases,
+    // as a side effect it breaks event continuation
+
+    this.selectionChanged(reference);
+  }
+
+  public void partDeactivated(final IWorkbenchPartReference reference) {
+    // ensures that pending events are flushed on part deactivation,
+    // this primarily handles proper selection termination on shutdown,
+    // as a side effect it breaks event continuation
+
     this.events.flush();
+  }
+
+  public void partVisible(final IWorkbenchPartReference reference) {
+    // ignore
+  }
+
+  public void partHidden(final IWorkbenchPartReference reference) {
+    // ignore
+  }
+
+  public void partBroughtToTop(final IWorkbenchPartReference reference) {
+    // ignore
+  }
+
+  public void partInputChanged(final IWorkbenchPartReference reference) {
+    // ignore
+  }
+
+  private void push(final long time, final IWorkbenchPart part, final ITextSelection selection) {
+    // eagerly filter out empty selections, no need to push event
+    // only to be filtered during acceptance phase by such simple operation
+
+    if (selection.isEmpty()) {
+      if (this.log.isEnabled()) {
+        this.log.print("%s: selection empty -> ignore", "selection");
+      }
+
+      return;
+    }
+
+    this.events.push(new TextSelectionEvent(time, part, selection));
+  }
+
+  public void selectionChanged(final IWorkbenchPartReference reference) {
+    final long time = this.currentTime();
+
+    IWorkbenchPart part = dereferencePart(reference);
+    ITextViewer viewer = TextViewerSupport.getTextViewer(part);
+
+    if (viewer == null) {
+      if (this.log.isEnabled()) {
+        this.log.print("%s: viewer not found for %s -> ignore", "selection", part);
+      }
+
+      return;
+    }
+
+    ISelection selection = viewer.getSelectionProvider().getSelection();
+
+    if (!(selection instanceof ITextSelection)) {
+      if (this.log.isEnabled()) {
+        this.log.print("%s: selection not textual in %s of %s -> ignore", "selection", viewer, part);
+      }
+
+      return;
+    }
+
+    this.push(time, part, (ITextSelection) selection);
   }
 
   public void selectionChanged(final IWorkbenchPart part, final ITextSelection selection) {
     final long time = this.currentTime();
 
-    if (selection.isEmpty()) {
-      return;
-    }
-
-    this.events.push(new TextSelectionEvent(time, part, selection));
+    this.push(time, part, selection);
   }
 }
