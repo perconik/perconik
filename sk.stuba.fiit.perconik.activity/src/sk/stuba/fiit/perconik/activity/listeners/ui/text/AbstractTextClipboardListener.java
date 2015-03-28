3d5c0da04dee9e5d1dc451245489b124c228170d
Pavol Zbell <pavol.zbell@gmail.com>
2015-03-28 16:08:44 +0100

    Support consoles in paste listener
    Sacrifice some paste assertions but enable event processing in consoles

---
+++ sk.stuba.fiit.perconik.activity/src/sk/stuba/fiit/perconik/activity/listeners/ui/text/AbstractTextClipboardListener.java
@@ -0,0 +1,49 @@
+package sk.stuba.fiit.perconik.activity.listeners.ui.text;
+
+import org.eclipse.jface.text.IDocument;
+import org.eclipse.jface.text.ITextViewer;
+import org.eclipse.swt.graphics.Point;
+import org.eclipse.ui.IWorkbenchPart;
+
+import sk.stuba.fiit.perconik.eclipse.jface.text.Documents;
+import sk.stuba.fiit.perconik.eclipse.jface.text.LineRegion;
+
+/**
+ * TODO
+ *
+ * @author Pavol Zbell
+ * @since 1.0
+ */
+abstract class AbstractTextClipboardListener extends AbstractTextListener {
+  AbstractTextClipboardListener() {}
+
+  final void process(final long time, final Action action) {
+    String text = this.execute(ClipboardReader.instance);
+
+    TextSelectionCapture capture = this.execute(TextSelectionReader.instance);
+
+    IWorkbenchPart part = capture.part;
+    ITextViewer viewer = capture.viewer;
+    IDocument document = viewer.getDocument();
+
+    Point range = capture.range;
+
+    int offset = range.x;
+    int length = range.y;
+
+    LineRegion region = LineRegion.compute(document, offset, length, text).normalize();
+    String selection = Documents.get(document, offset, length);
+
+    this.process(time, action, part, document, region, selection);
+  }
+
+  final void process(final long time, final Action action, final IWorkbenchPart part, final IDocument document, final LineRegion region, final String selection) {
+    if (!this.validate(part, document, region, selection)) {
+      return;
+    }
+
+    this.process(time, action, part, region);
+  }
+
+  abstract boolean validate(final IWorkbenchPart part, final IDocument document, final LineRegion region, final String selection);
+}
--- sk.stuba.fiit.perconik.activity/src/sk/stuba/fiit/perconik/activity/listeners/ui/text/TextPasteListener.java
+++ sk.stuba.fiit.perconik.activity/src/sk/stuba/fiit/perconik/activity/listeners/ui/text/TextPasteListener.java
@@ -5,28 +5,17 @@ import org.eclipse.core.commands.ExecutionException;
 import org.eclipse.core.commands.NotEnabledException;
 import org.eclipse.core.commands.NotHandledException;
 import org.eclipse.core.commands.common.NotDefinedException;
-import org.eclipse.jface.text.DocumentEvent;
 import org.eclipse.jface.text.IDocument;
-import org.eclipse.ui.IEditorPart;
+import org.eclipse.ui.IWorkbenchPart;
 
 import sk.stuba.fiit.perconik.activity.listeners.ActivityListener;
 import sk.stuba.fiit.perconik.core.annotations.Version;
 import sk.stuba.fiit.perconik.core.listeners.CommandExecutionListener;
-import sk.stuba.fiit.perconik.core.listeners.DocumentListener;
-import sk.stuba.fiit.perconik.eclipse.core.commands.CommandExecutionStateHandler;
 import sk.stuba.fiit.perconik.eclipse.jface.text.LineRegion;
-import sk.stuba.fiit.perconik.eclipse.ui.Editors;
 
 import static java.util.Objects.requireNonNull;
 
 import static sk.stuba.fiit.perconik.activity.listeners.ui.text.TextPasteListener.Action.PASTE;
-import static sk.stuba.fiit.perconik.eclipse.core.commands.CommandExecutionState.DISABLED;
-import static sk.stuba.fiit.perconik.eclipse.core.commands.CommandExecutionState.EXECUTING;
-import static sk.stuba.fiit.perconik.eclipse.core.commands.CommandExecutionState.FAILED;
-import static sk.stuba.fiit.perconik.eclipse.core.commands.CommandExecutionState.SUCCEEDED;
-import static sk.stuba.fiit.perconik.eclipse.core.commands.CommandExecutionState.UNDEFINED;
-import static sk.stuba.fiit.perconik.eclipse.core.commands.CommandExecutionState.UNHANDLED;
-import static sk.stuba.fiit.perconik.utilities.MoreStrings.toLowerCase;
 
 /**
  * TODO
@@ -34,13 +23,9 @@ import static sk.stuba.fiit.perconik.utilities.MoreStrings.toLowerCase;
  * @author Pavol Zbell
  * @since 1.0
  */
-@Version("0.0.2.alpha")
-public final class TextPasteListener extends AbstractTextListener implements CommandExecutionListener, DocumentListener {
-  private final CommandExecutionStateHandler paste;
-
-  public TextPasteListener() {
-    this.paste = CommandExecutionStateHandler.of(PASTE.getIdentifier());
-  }
+@Version("0.0.3.alpha")
+public final class TextPasteListener extends AbstractTextClipboardListener implements CommandExecutionListener {
+  public TextPasteListener() {}
 
   enum Action implements ActivityListener.Action {
     PASTE("org.eclipse.ui.edit.paste");
@@ -71,66 +56,48 @@ public final class TextPasteListener extends AbstractTextListener implements Com
     }
   }
 
-  void process(final long time, final Action action, final DocumentEvent event) {
-    IDocument document = event.getDocument();
-    IEditorPart editor = Editors.forDocument(document);
+  @Override
+  boolean validate(final IWorkbenchPart part, final IDocument document, final LineRegion region, final String selection) {
+    boolean valid = region.text != null && selection.isEmpty();
 
-    LineRegion region = LineRegion.compute(document, event.getOffset(), event.getLength(), event.getText()).normalize();
-
-    if (editor == null) {
-      if (this.log.isEnabled()) {
-        this.log.print("%s: editor not found / documents not equal", "paste");
-      }
-
-      return;
+    if (!valid && this.log.isEnabled()) {
+      this.log.print("%s: part selection not empty '%s'", "paste", selection);
     }
 
-    this.process(time, action, editor, region);
+    return valid;
   }
 
-  public void documentAboutToBeChanged(final DocumentEvent event) {
+  public void preExecute(final String identifier, final ExecutionEvent event) {
     // ignore
   }
 
-  public void documentChanged(final DocumentEvent event) {
+  public void postExecuteSuccess(final String identifier, final Object result) {
     final long time = this.currentTime();
 
-    if (this.paste.getState() != EXECUTING) {
-      if (this.log.isEnabled()) {
-        this.log.print("%s: not executing but %s", "paste", toLowerCase(this.paste.getState()));
-      }
-
+    if (!PASTE.getIdentifier().equals(identifier)) {
       return;
     }
 
     this.execute(new Runnable() {
       public void run() {
-        process(time, PASTE, event);
+        process(time, PASTE);
       }
     });
   }
 
-  public void preExecute(final String identifier, final ExecutionEvent event) {
-    this.paste.transitOnMatch(identifier, EXECUTING);
-  }
-
-  public void postExecuteSuccess(final String identifier, final Object result) {
-    this.paste.transitOnMatch(identifier, SUCCEEDED);
-  }
-
   public void postExecuteFailure(final String identifier, final ExecutionException exception) {
-    this.paste.transitOnMatch(identifier, FAILED);
+    // ignore
   }
 
   public void notDefined(final String identifier, final NotDefinedException exception) {
-    this.paste.transitOnMatch(identifier, UNDEFINED);
+    // ignore
   }
 
   public void notEnabled(final String identifier, final NotEnabledException exception) {
-    this.paste.transitOnMatch(identifier, DISABLED);
+    // ignore
   }
 
   public void notHandled(final String identifier, final NotHandledException exception) {
-    this.paste.transitOnMatch(identifier, UNHANDLED);
+    // ignore
   }
 }
