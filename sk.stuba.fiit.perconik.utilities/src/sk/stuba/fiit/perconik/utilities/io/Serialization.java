ace9790bf23db805d3d3dfa76413bf988336be05
Pavol Zbell <pavol.zbell@gmail.com>
2015-03-31 12:40:33 +0200

    Add correct annotations

--- sk.stuba.fiit.perconik.utilities/src/sk/stuba/fiit/perconik/utilities/io/Serialization.java
+++ sk.stuba.fiit.perconik.utilities/src/sk/stuba/fiit/perconik/utilities/io/Serialization.java
@@ -7,13 +7,15 @@ import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.nio.charset.Charset;
 
+import javax.annotation.Nullable;
+
 import sk.stuba.fiit.perconik.utilities.reflect.resolver.ClassResolver;
 import sk.stuba.fiit.perconik.utilities.reflect.resolver.ClassResolvers;
 
 public final class Serialization {
   private Serialization() {}
 
-  private static Object read(final byte[] data, final ClassResolver resolver) throws ClassNotFoundException, IOException {
+  private static Object read(@Nullable final byte[] data, final ClassResolver resolver) throws ClassNotFoundException, IOException {
     if (data == null || data.length == 0) {
       return null;
     }
@@ -25,24 +27,24 @@ public final class Serialization {
     }
   }
 
-  public static Object fromBytes(final byte[] data) throws ClassNotFoundException, IOException {
+  public static Object fromBytes(@Nullable final byte[] data) throws ClassNotFoundException, IOException {
     return fromBytes(data, ClassResolvers.getDefault());
   }
 
-  public static Object fromBytes(final byte[] data, final ClassResolver resolver) throws ClassNotFoundException, IOException {
+  public static Object fromBytes(@Nullable final byte[] data, final ClassResolver resolver) throws ClassNotFoundException, IOException {
     return read(data, resolver);
   }
 
-  public static Object fromString(final String data) throws ClassNotFoundException, IOException {
-    return fromBytes(data.getBytes());
+  public static Object fromString(@Nullable final String data) throws ClassNotFoundException, IOException {
+    return fromBytes(data != null ? data.getBytes() : null);
   }
 
-  public static Object fromString(final String data, final Charset charset) throws ClassNotFoundException, IOException {
-    return fromBytes(data.getBytes(charset));
+  public static Object fromString(@Nullable final String data, final Charset charset) throws ClassNotFoundException, IOException {
+    return fromBytes(data != null ? data.getBytes(charset) : null);
   }
 
-  public static Object fromString(final String data, final Charset charset, final ClassResolver resolver) throws ClassNotFoundException, IOException {
-    return fromBytes(data.getBytes(charset), resolver);
+  public static Object fromString(@Nullable final String data, final Charset charset, final ClassResolver resolver) throws ClassNotFoundException, IOException {
+    return fromBytes(data != null ? data.getBytes(charset) : null, resolver);
   }
 
   private static ByteArrayOutputStream write(final Object object) throws IOException {
@@ -56,11 +58,11 @@ public final class Serialization {
     }
   }
 
-  public static byte[] toBytes(final Object object) throws IOException {
+  public static byte[] toBytes(@Nullable final Object object) throws IOException {
     return write(object).toByteArray();
   }
 
-  public static String toString(final Object object) throws IOException {
+  public static String toString(@Nullable final Object object) throws IOException {
     return write(object).toString();
   }
 }
