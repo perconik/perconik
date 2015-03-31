5d5931f6df560dbcf4c93361fd08716aff4c0542
Pavol Zbell <pavol.zbell@gmail.com>
2015-03-31 21:22:10 +0200

    Adjust debug output

--- sk.stuba.fiit.perconik.activity.debug/src/sk/stuba/fiit/perconik/activity/debug/listeners/AbstractLifecycleListener.java
+++ sk.stuba.fiit.perconik.activity.debug/src/sk/stuba/fiit/perconik/activity/debug/listeners/AbstractLifecycleListener.java
@@ -2,28 +2,28 @@ package sk.stuba.fiit.perconik.activity.debug.listeners;
 
 import javax.annotation.Nullable;
 
-import jersey.repackaged.com.google.common.base.Joiner;
-
 import sk.stuba.fiit.perconik.activity.listeners.ActivityListener;
-import sk.stuba.fiit.perconik.core.debug.annotations.DebugImplementation;
 
-@DebugImplementation
+import static java.util.Arrays.asList;
+
+import static sk.stuba.fiit.perconik.utilities.MoreStrings.toDefaultString;
+
 public abstract class AbstractLifecycleListener extends ActivityListener {
   public AbstractLifecycleListener() {}
 
-  private void report(final Object subject, final String prefix, final String format, @Nullable final Object ... args) {
-    this.log.print("%s: %s -> %s", prefix, subject, String.format(format, args));
-  }
-
   protected final void mark(final Object subject, final String prefix, final String format, @Nullable final Object ... args) {
-    if (this.log.isEnabled()) {
-      this.report(subject, prefix, format, args);
-    }
+    this.mark(asList(subject), prefix, format, args);
   }
 
   protected final void mark(final Iterable<?> subjects, final String prefix, final String format, @Nullable final Object ... args) {
     if (this.log.isEnabled()) {
-      this.report(Joiner.on(", ").join(subjects), prefix, format, args);
+      StringBuilder builder = new StringBuilder();
+
+      for (Object subject: subjects) {
+        builder.append(toDefaultString(subject)).append(", ");
+      }
+
+      this.log.print("%s: %s -> %s", prefix, builder.substring(0, builder.length() - 2), String.format(format, args));
     }
   }
 }
