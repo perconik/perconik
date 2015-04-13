1ef453150cc79acb759626f7f4ef8a71e5e6bd15
Pavol Zbell <pavol.zbell@gmail.com>
2015-04-14 01:26:05 +0200

    Add some documentation

--- com.gratex.perconik.uaca/src/com/gratex/perconik/uaca/GenericUacaEventConstants.java
+++ com.gratex.perconik.uaca/src/com/gratex/perconik/uaca/GenericUacaEventConstants.java
@@ -5,8 +5,14 @@ import java.net.URI;
 import static sk.stuba.fiit.perconik.utilities.net.UniformResources.newUri;
 
 public class GenericUacaEventConstants {
+  /**
+   * UACA event type URI prefix, value of {@code http://perconik.gratex.com/useractivity}
+   */
   public static final URI EVENT_TYPE_URI_PREFIX = newUri("http://perconik.gratex.com/useractivity");
 
+  /**
+   * UACA event timestamp pattern, value of {@code yyyy-MM-dd'T'HH:mm:ss.SSSXXX}
+   */
   public static final String TIMESTAMP_PATTERN = "yyyy-MM-dd'T'HH:mm:ss.SSSXXX";
 
   private GenericUacaEventConstants() {}
