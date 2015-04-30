440e6ce9114062fac3aafc1bca57c3289fd5926a
Pavol Zbell <pavol.zbell@gmail.com>
2015-04-30 02:25:58 +0200

    Remove incorrect Javadoc

--- sk.stuba.fiit.perconik.eclipse/src/sk/stuba/fiit/perconik/eclipse/jface/preference/DefaultPreferenceStore.java
+++ sk.stuba.fiit.perconik.eclipse/src/sk/stuba/fiit/perconik/eclipse/jface/preference/DefaultPreferenceStore.java
@@ -2,25 +2,10 @@ package sk.stuba.fiit.perconik.eclipse.jface.preference;
 
 import javax.annotation.Nullable;
 
-import com.google.common.collect.ForwardingObject;
-
 import org.eclipse.jface.preference.IPreferenceStore;
 
 import static com.google.common.base.Preconditions.checkNotNull;
 
-/**
- * A forwarding preference store which forwards all its value accessor method
- * calls to default value accessor methods of another preference store.
- * Subclasses should override one or more methods to modify the behavior
- * of the backing preference store as desired per the decorator pattern.
- *
- * <p>Note that this class does <i>not</i> forward the {@code equals}
- * and {@code hashCode} methods through to the backing object.
- * See {@link ForwardingObject} for more details.
- *
- * @author Pavol Zbell
- * @since 1.0
- */
 public final class DefaultPreferenceStore extends ForwardingPreferenceStore {
   private final IPreferenceStore store;
 
