cddb9e46c4c845d141f69151b46b8e2d3969c011
Pavol Zbell <pavol.zbell@gmail.com>
2015-05-27 17:55:15 +0200

    Do not return optional as ElasticsearchProxy execution result

--- sk.stuba.fiit.perconik.elasticsearch/src/sk/stuba/fiit/perconik/elasticsearch/AbstractElasticsearchProxy.java
+++ sk.stuba.fiit.perconik.elasticsearch/src/sk/stuba/fiit/perconik/elasticsearch/AbstractElasticsearchProxy.java
@@ -2,30 +2,25 @@ package sk.stuba.fiit.perconik.elasticsearch;
 
 import javax.annotation.Nullable;
 
-import com.google.common.base.Optional;
-
 import org.elasticsearch.client.transport.TransportClient;
 
 import static java.lang.String.format;
-
-import static com.google.common.base.Optional.absent;
-import static com.google.common.base.Optional.of;
 
 public abstract class AbstractElasticsearchProxy implements ElasticsearchProxy {
   public AbstractElasticsearchProxy() {}
 
   protected abstract TransportClient client();
 
-  public final <T> Optional<T> execute(final Task<T> task) {
+  public final <T> T execute(final Task<T> task) {
     final TransportClient client = this.client();
 
     try {
-      return of(task.perform(client));
+      return task.perform(client);
     } catch (Exception failure) {
       this.reportFailure(format("unexpected failure while performing %s on %s", task, client), failure);
-    }
 
-    return absent();
+      throw failure;
+    }
   }
 
   @SuppressWarnings("unused")
--- sk.stuba.fiit.perconik.elasticsearch/src/sk/stuba/fiit/perconik/elasticsearch/ElasticsearchProxy.java
+++ sk.stuba.fiit.perconik.elasticsearch/src/sk/stuba/fiit/perconik/elasticsearch/ElasticsearchProxy.java
@@ -1,7 +1,5 @@
 package sk.stuba.fiit.perconik.elasticsearch;
 
-import com.google.common.base.Optional;
-
 import org.elasticsearch.client.transport.TransportClient;
 import org.elasticsearch.common.settings.Settings;
 
@@ -10,7 +8,7 @@ public interface ElasticsearchProxy extends AutoCloseable {
     public T perform(TransportClient client);
   }
 
-  public <T> Optional<T> execute(Task<T> task);
+  public <T> T execute(Task<T> task);
 
   public Settings settings();
 }
