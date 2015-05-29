7b8efb62ee85cd87223529b3b23b4a1bfa0b4035
Pavol Zbell <pavol.zbell@gmail.com>
2015-05-29 14:14:14 +0200

    Improve preference store helpers

--- sk.stuba.fiit.perconik.eclipse/src/sk/stuba/fiit/perconik/eclipse/jface/preference/PreferenceStores.java
+++ sk.stuba.fiit.perconik.eclipse/src/sk/stuba/fiit/perconik/eclipse/jface/preference/PreferenceStores.java
@@ -3,26 +3,28 @@ package sk.stuba.fiit.perconik.eclipse.jface.preference;
 import java.util.Map;
 import java.util.Map.Entry;
 
+import com.google.common.base.Function;
+
 import org.eclipse.jface.preference.IPreferenceStore;
 
 public final class PreferenceStores {
   private PreferenceStores() {}
 
-  public static final void put(final IPreferenceStore store, final Map<String, ?> preferences) {
+  public static final void put(final IPreferenceStore store, final Map<String, ?> preferences, final Function<Object, String> converter) {
     for (Entry<String, ?> entry: preferences.entrySet()) {
-      store.putValue(entry.getKey(), entry.getValue().toString());
+      store.putValue(entry.getKey(), converter.apply(entry.getValue()));
     }
   }
 
-  public static final void set(final IPreferenceStore store, final Map<String, ?> preferences) {
+  public static final void set(final IPreferenceStore store, final Map<String, ?> preferences, final Function<Object, String> converter) {
     for (Entry<String, ?> entry: preferences.entrySet()) {
-      store.setValue(entry.getKey(), entry.getValue().toString());
+      store.setValue(entry.getKey(), converter.apply(entry.getValue()));
     }
   }
 
-  public static final void setDefault(final IPreferenceStore store, final Map<String, ?> preferences) {
+  public static final void setDefault(final IPreferenceStore store, final Map<String, ?> preferences, final Function<Object, String> converter) {
     for (Entry<String, ?> entry: preferences.entrySet()) {
-      store.setDefault(entry.getKey(), entry.getValue().toString());
+      store.setDefault(entry.getKey(), converter.apply(entry.getValue()));
     }
   }
 }
