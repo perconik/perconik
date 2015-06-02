2730a8cf5b2ee6b5ead692dc7361ade71028e0a3
Pavol Zbell <pavol.zbell@gmail.com>
2015-06-02 19:57:07 +0200

    Correct behavior in MorePreconditions

--- sk.stuba.fiit.perconik.utilities/src/sk/stuba/fiit/perconik/utilities/MorePreconditions.java
+++ sk.stuba.fiit.perconik.utilities/src/sk/stuba/fiit/perconik/utilities/MorePreconditions.java
@@ -4,43 +4,45 @@ import java.util.Collection;
 
 import javax.annotation.Nullable;
 
+import static com.google.common.base.Preconditions.checkArgument;
+import static com.google.common.base.Preconditions.checkNotNull;
 import static com.google.common.base.Preconditions.checkState;
 
 public final class MorePreconditions {
   private MorePreconditions() {}
 
   public static <T extends CharSequence> T checkNotNullOrEmpty(final T reference) {
-    checkState(reference.length() > 0);
+    checkArgument(checkNotNull(reference).length() > 0);
 
     return reference;
   }
 
   public static <T extends CharSequence> T checkNotNullOrEmpty(final T reference, @Nullable final Object message) {
-    checkState(reference.length() > 0, message);
+    checkArgument(checkNotNull(reference, message).length() > 0, message);
 
     return reference;
   }
 
   public static <T extends CharSequence> T checkNotNullOrEmpty(final T reference, @Nullable final String format, @Nullable final Object ... args) {
-    checkState(reference.length() > 0, format, args);
+    checkArgument(checkNotNull(reference, format, args).length() > 0, format, args);
 
     return reference;
   }
 
   public static <T extends Collection<?>> T checkNotNullOrEmpty(final T reference) {
-    checkState(!reference.isEmpty());
+    checkArgument(!checkNotNull(reference).isEmpty());
 
     return reference;
   }
 
   public static <T extends Collection<?>> T checkNotNullOrEmpty(final T reference, @Nullable final Object message) {
-    checkState(!reference.isEmpty(), message);
+    checkArgument(!checkNotNull(reference, message).isEmpty(), message);
 
     return reference;
   }
 
   public static <T extends Collection<?>> T checkNotNullOrEmpty(final T reference, @Nullable final String format, @Nullable final Object ... args) {
-    checkState(!reference.isEmpty(), format, args);
+    checkArgument(!checkNotNull(reference, format, args).isEmpty(), format, args);
 
     return reference;
   }
